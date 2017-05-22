f64f93b2e8c901b80e56f61fc7bcba9a719a0844
Alexander Gabriel <alex@gabriel-software.ch>
2017-05-22 11:37:29 +0200

    install material

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react'
-import './App.css'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <h2>Welcome to arteigenschaften</h2>
+      </MuiThemeProvider>
     )
   }
 }
