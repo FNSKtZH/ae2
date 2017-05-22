ad96653fb4056b9ae8e2de26a6507840fdd29034
Alexander Gabriel <alex@gabriel-software.ch>
2017-05-22 22:01:07 +0200

    try to get relay to fetch data

--- src/components/Taxonomy.js
+++ src/components/Taxonomy.js
@@ -1,6 +1,30 @@
 // @flow
 import React from 'react'
+import { QueryRenderer, graphql } from 'react-relay'
 
-const Taxonomy = () => <div>Taxonomie</div>
+import environment from '../modules/createRelayEnvironment'
+
+const Taxonomy = () => {
+  return (
+    <div>
+      Taxonomie
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+            query Taxonomy {
+              allCategories {
+                nodes {
+                  name
+                }
+              }
+            }
+          `}
+        render={(error, props) => {
+          console.log('props:', props)
+        }}
+      />
+    </div>
+  )
+}
 
 export default Taxonomy
