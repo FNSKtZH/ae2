a0c320f55c49abf3e7c737645e9d1f18a2c367c1
Alexander Gabriel <alex@gabriel-software.ch>
2017-05-26 21:55:26 +0200

    add actions to set nodes

--- src/store/extend/nodes.js
+++ src/store/extend/nodes.js
@@ -1,5 +1,5 @@
 // @flow
-import { extendObservable } from 'mobx'
+import { extendObservable, action } from 'mobx'
 
 import topLevelNodes from '../modules/nodes/topLevel'
 
@@ -7,7 +7,16 @@ export default (store: Object): void => {
   extendObservable(store.nodes, {
     topLevel: topLevelNodes,
     taxCategories: [],
+    setTaxCategoriesNodes: action('setTaxCategoriesNodes', nodes => {
+      store.nodes.taxCategories = nodes
+    }),
     taxTaxonomies: [],
+    setTaxTaxonomiesNodes: action('setTaxTaxonomiesNodes', nodes => {
+      store.nodes.taxTaxonomies = nodes
+    }),
     taxTaxonomyObjects: [],
+    setTaxTaxonomyObjectsNodes: action('setTaxTaxonomyObjectsNodes', nodes => {
+      store.nodes.TaxonomyObjects = nodes
+    }),
   })
 }
