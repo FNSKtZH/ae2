7dad8cbedf365c8f18c33699385f367de9c7a473
Alexander Gabriel <alex@gabriel-software.ch>
2017-05-31 12:04:14 +0200

    work on tree

--- sql/taxonomy_taxonomy_object_level1.sql
+++ sql/taxonomy_taxonomy_object_level1.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION ae.taxonomy_taxonomy_object_level1(taxonomy ae.taxonomy, name text)
+CREATE OR REPLACE FUNCTION ae.taxonomy_taxonomy_object_level1(taxonomy ae.taxonomy, taxonomy_id uuid)
   RETURNS setof ae.taxonomy_object AS
   $$
     SELECT to1.*
@@ -9,10 +9,10 @@ CREATE OR REPLACE FUNCTION ae.taxonomy_taxonomy_object_level1(taxonomy ae.taxono
       to1.parent_id IS NULL AND
       1 = CASE
         WHEN $2 IS NULL THEN 1
-        WHEN ae.taxonomy.name = $2 THEN 1
+        WHEN ae.taxonomy.id = $2 THEN 1
         ELSE 2
       END
   $$
   LANGUAGE sql STABLE;
-ALTER FUNCTION ae.taxonomy_taxonomy_object_level1(taxonomy ae.taxonomy, name text)
+ALTER FUNCTION ae.taxonomy_taxonomy_object_level1(taxonomy ae.taxonomy, taxonomy_id uuid)
   OWNER TO postgres;
