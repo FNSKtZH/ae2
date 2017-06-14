987d0a58ab0c2efa1dc3c918f28c20afab7bee60
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-14 15:52:31 +0200

    work on taxonomy

--- src/components/Objekt/Taxonomy.js
+++ src/components/Objekt/Taxonomy.js
@@ -2,13 +2,24 @@
 import React from 'react'
 import { observer, inject } from 'mobx-react'
 import compose from 'recompose/compose'
+import {
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText,
+} from 'material-ui/Card'
 import styled from 'styled-components'
+import get from 'lodash/get'
 
 const enhance = compose(inject('store'), observer)
 
 const Taxonomy = ({ store, taxonomy }: { store: Object, taxonomy: Object }) => {
   console.log('Taxonomy:', taxonomy)
-  return <div>{taxonomy.name}</div>
+  const tax = get(taxonomy, 'taxonomyByTaxonomyId', {})
+  const taxName = tax.name || '(Name fehlt)'
+  return <Card>{taxName}</Card>
 }
 
 export default enhance(Taxonomy)
