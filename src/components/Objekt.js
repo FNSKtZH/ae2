619e93affb93fe96e5e422cfead4e256ad6d887d
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-14 13:26:43 +0200

    add titles

--- src/components/Objekt.js
+++ src/components/Objekt.js
@@ -4,18 +4,45 @@ import { toJS } from 'mobx'
 import { observer, inject } from 'mobx-react'
 import compose from 'recompose/compose'
 import styled from 'styled-components'
+import get from 'lodash/get'
 
 const Container = styled.div`
+  padding: 5px;
   height: calc(100% - 48px);
   overflow: auto !important;
+`
+const Title = styled.h3`
+
 `
 
 const enhance = compose(inject('store'), observer)
 
 const Objekt = ({ store }: { store: Object }) => {
+  const { activeTaxonomyObject } = store
   console.log('activeTaxonomyObject:', toJS(store.activeTaxonomyObject))
-  const to = JSON.stringify(toJS(store.activeTaxonomyObject), null, 2)
-  return <Container><div>Objekt</div><div>{to}</div></Container>
+  const taxCount = get(
+    activeTaxonomyObject,
+    'taxonomyObjectsByObjectId.totalCount',
+    0,
+  )
+  const pcCount = get(
+    activeTaxonomyObject,
+    'propertyCollectionObjectsByObjectId.totalCount',
+    0,
+  )
+  const rcCount = get(
+    activeTaxonomyObject,
+    'relationCollectionObjectsByObjectId.totalCount',
+    0,
+  )
+
+  return (
+    <Container>
+      <Title>{`Taxonomien (${taxCount}):`}</Title>
+      <Title>{`Eigenschaften-Sammlungen (${pcCount}):`}</Title>
+      <Title>{`Beziehungs-Sammlungen (${rcCount}):`}</Title>
+    </Container>
+  )
 }
 
 export default enhance(Objekt)
