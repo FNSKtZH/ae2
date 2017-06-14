d15c1579d83a9ee6068caaeb73481dea4c44e6b7
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-14 13:33:10 +0200

    refactor

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { observer, inject } from 'mobx-react'
 import compose from 'recompose/compose'
 
-import Objekt from './Objekt'
+import Objekt from './Objekt/index'
 import PropertyCollection from './PropertyCollection'
 import RelationCollection from './RelationCollection'
 import Exporte from './Exporte'
