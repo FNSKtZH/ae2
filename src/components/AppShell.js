06c21416d4d02dd74b17f640f6514576111c4a23
Alexander Gabriel <alex@gabriel-software.ch>
2017-10-10 23:10:59 +0200

    optimize

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -4,13 +4,14 @@
  * every time activeNodeArray changes.
  * Result: data is requeried.
  */
-import React, { Component } from 'react'
+import React from 'react'
 import { observer, inject } from 'mobx-react'
 import compose from 'recompose/compose'
 
 import App from './App'
 
 const enhance = compose(inject('store'), observer)
+
 const AppShell = ({ store }: { store: Object }) => (
   <App activeNodeArrayString={store.activeNodeArray.join()} />
 )
