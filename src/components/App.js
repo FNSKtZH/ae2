57c25aa483b8b1068a0bad72dda2605240a1da8f
Alexander Gabriel <alex@gabriel-software.ch>
2017-11-11 22:10:31 +0100

    format

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
 // @flow
 import React from 'react'
 import styled from 'styled-components'
-import {
-  observer,
-  inject
-} from 'mobx-react'
+import { observer, inject } from 'mobx-react'
 //import { toJS } from 'mobx'
 import compose from 'recompose/compose'
 import Snackbar from 'material-ui/Snackbar'
@@ -19,7 +16,7 @@ import Organisation from './Organisation'
 import Login from './Login'
 import FourOhFour from './FourOhFour'
 
-const Container = styled.div `
+const Container = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -27,84 +24,56 @@ const Container = styled.div `
 
 const enhance = compose(inject('store'), AppData, observer)
 
-const App = ({
-    store,
-    data
-  }: {
-    store: Object,
-    data: Object
-  }) => {
-    const {
-      activeObject,
-      error,
-      loading
-    } = data
-    console.log('App, render: data:', data)
-    // console.log('App, render: activeNodeArray:', toJS(store.activeNodeArray))
-    store.setProps(data)
+const App = ({ store, data }: { store: Object, data: Object }) => {
+  const { activeObject, error, loading } = data
+  console.log('App, render: data:', data)
+  // console.log('App, render: activeNodeArray:', toJS(store.activeNodeArray))
+  store.setProps(data)
 
-    const url0 =
-      store.activeNodeArray[0] && store.activeNodeArray[0].toLowerCase()
-    const url1 =
-      store.activeNodeArray[1] && store.activeNodeArray[1].toLowerCase()
-    const show404 = ![
-        'taxonomien',
-        'eigenschaften-sammlungen',
-        'organisationen',
-        'export',
-        'import',
-        'login',
-      ].includes(url0) ||
-      (url0 === 'import' &&
-        !['eigenschaften-sammlungen', 'beziehungs-sammlungen'].includes(url1))
-    const showData = ['taxonomien', 'eigenschaften-sammlungen'].includes(url0)
-    const showExport = url0 === 'export'
-    const showOrganisation = url0 === 'organisationen'
-    const showLogin = url0 === 'login'
-    const showImportPc = url0 === 'import' && url1 === 'eigenschaften-sammlungen'
-    const showImportRc = url0 === 'import' && url1 === 'beziehungs-sammlungen'
+  const url0 =
+    store.activeNodeArray[0] && store.activeNodeArray[0].toLowerCase()
+  const url1 =
+    store.activeNodeArray[1] && store.activeNodeArray[1].toLowerCase()
+  const show404 =
+    ![
+      'taxonomien',
+      'eigenschaften-sammlungen',
+      'organisationen',
+      'export',
+      'import',
+      'login',
+    ].includes(url0) ||
+    (url0 === 'import' &&
+      !['eigenschaften-sammlungen', 'beziehungs-sammlungen'].includes(url1))
+  const showData = ['taxonomien', 'eigenschaften-sammlungen'].includes(url0)
+  const showExport = url0 === 'export'
+  const showOrganisation = url0 === 'organisationen'
+  const showLogin = url0 === 'login'
+  const showImportPc = url0 === 'import' && url1 === 'eigenschaften-sammlungen'
+  const showImportRc = url0 === 'import' && url1 === 'beziehungs-sammlungen'
 
-    return ( <
-        Container >
-        <
-        AppBar / >
-        <
-        Snackbar open = {
-          loading
-        }
-        message = "lade Daten..."
-        bodyStyle = {
-          {
-            maxWidth: 100,
-            minWidth: 100,
-            backgroundColor: 'rgb(217, 78, 0)',
-          }
-        }
-        /> {
-          error && < div > {
-            error.message
-          } < /div>} {
-            showData && < Data data = {
-              data
-            }
-            activeObject = {
-              activeObject
-            }
-            />} {
-              showExport && < Export / >
-            } {
-              showImportPc && < ImportPc / >
-            } {
-              showImportRc && < ImportRc / >
-            } {
-              showOrganisation && < Organisation / >
-            } {
-              showLogin && < Login / >
-            } {
-              show404 && < FourOhFour / >
-            } <
-            /Container>
-          )
-        }
+  return (
+    <Container>
+      <AppBar />
+      <Snackbar
+        open={loading}
+        message="lade Daten..."
+        bodyStyle={{
+          maxWidth: 100,
+          minWidth: 100,
+          backgroundColor: 'rgb(217, 78, 0)',
+        }}
+      />
+      {error && <div> {error.message} </div>}
+      {showData && <Data data={data} activeObject={activeObject} />}
+      {showExport && <Export />}
+      {showImportPc && <ImportPc />}
+      {showImportRc && <ImportRc />}
+      {showOrganisation && <Organisation />}
+      {showLogin && <Login />}
+      {show404 && <FourOhFour />}
+    </Container>
+  )
+}
 
-        export default enhance(App)
+export default enhance(App)
