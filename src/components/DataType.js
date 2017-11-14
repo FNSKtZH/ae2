4085590fc8e4e9a02e35bd32e49c57f3d7639a60
Alexander Gabriel <alex@gabriel-software.ch>
2017-11-14 23:42:46 +0100

    reduce use of getActiveNodeArray

--- src/components/DataType.js
+++ src/components/DataType.js
@@ -1,22 +1,30 @@
 // @flow
 import React from 'react'
 import { /*observer,*/ inject } from 'mobx-react'
+import { graphql } from 'react-apollo'
 import compose from 'recompose/compose'
 
 import Objekt from './Objekt'
 import PropertyCollection from './PropertyCollection'
-import getActiveNodeArray from '../modules/getActiveNodeArray'
+import activeNodeArrayGql from '../modules/activeNodeArrayGql'
 
-const enhance = compose(inject('store') /*, observer*/)
+const activeNodeArrayData = graphql(activeNodeArrayGql, {
+  name: 'activeNodeArrayData',
+})
+
+const enhance = compose(inject('store'), activeNodeArrayData /*, observer*/)
 
 const DataType = ({
   store,
   activeObject,
+  activeNodeArrayData,
 }: {
   store: Object,
   activeObject: Object,
+  activeNodeArrayData: Object,
 }) => {
-  const activeNodeArray = getActiveNodeArray()
+  const activeNodeArray =
+    activeNodeArrayData && activeNodeArrayData.activeNodeArray[0].value
   const primaryUrl = activeNodeArray[0]
 
   return (
