b48292aa91e750765e08285e97133384b1b95564
Alexander Gabriel <alex@gabriel-software.ch>
2017-11-14 23:40:13 +0100

    reduce use of getActiveNodeArray

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,32 +2,43 @@
 import React from 'react'
 import { ReflexContainer, ReflexSplitter, ReflexElement } from 'react-reflex'
 import { observer, inject } from 'mobx-react'
+import { graphql } from 'react-apollo'
 import compose from 'recompose/compose'
 
 import Tree from './Tree'
 import DataType from './DataType'
-import getActiveNodeArray from '../modules/getActiveNodeArray'
+import activeNodeArrayGql from '../modules/activeNodeArrayGql'
 
-const enhance = compose(inject('store'), observer)
+const activeNodeArrayData = graphql(activeNodeArrayGql, {
+  name: 'activeNodeArrayData',
+})
+
+const enhance = compose(inject('store'), activeNodeArrayData, observer)
 
 const Data = ({
   store,
   data,
+  activeNodeArrayData,
   activeObject,
 }: {
   store: Object,
   data: Object,
+  activeNodeArrayData: Object,
   activeObject: Object,
-}) => (
-  <ReflexContainer orientation="vertical">
-    <ReflexElement flex={0.35} className="tree-reflex-element">
-      <Tree data={data} activeNodeArray={getActiveNodeArray()} />
-    </ReflexElement>
-    <ReflexSplitter key="treeSplitter" />
-    <ReflexElement>
-      <DataType activeObject={activeObject} />
-    </ReflexElement>
-  </ReflexContainer>
-)
+}) => {
+  const activeNodeArray =
+    activeNodeArrayData && activeNodeArrayData.activeNodeArray[0].value
+  return (
+    <ReflexContainer orientation="vertical">
+      <ReflexElement flex={0.35} className="tree-reflex-element">
+        <Tree data={data} activeNodeArray={activeNodeArray} />
+      </ReflexElement>
+      <ReflexSplitter key="treeSplitter" />
+      <ReflexElement>
+        <DataType activeObject={activeObject} />
+      </ReflexElement>
+    </ReflexContainer>
+  )
+}
 
 export default enhance(Data)
