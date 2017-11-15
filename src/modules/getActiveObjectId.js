d35a2c599382e3911f121a0a43358c72ea1721d7
Alexander Gabriel <alex@gabriel-software.ch>
2017-11-15 21:44:18 +0100

    modularize

--- src/modules/getActiveObjectId.js
+++ src/modules/getActiveObjectId.js
@@ -1,11 +1,9 @@
 // @flow
 
 import getActiveNodeArray from './getActiveNodeArray'
+import getActiveObjectIdFromNodeArray from './getActiveObjectIdFromNodeArray'
 
 export default (): string => {
   const activeNodeArray = getActiveNodeArray()
-  if (activeNodeArray.length > 2 && activeNodeArray[0] === 'Taxonomien') {
-    return activeNodeArray[activeNodeArray.length - 1]
-  }
-  return '99999999-9999-9999-9999-999999999999'
+  return getActiveObjectIdFromNodeArray(activeNodeArray)
 }
