de729a4e509797cbf94286cb9e72459eff457ad2
Alexander Gabriel <alex@gabriel-software.ch>
2017-11-16 20:43:06 +0100

    remove unneeded

--- src/store/extend/treeFilter.js
+++ src/store/extend/treeFilter.js
@@ -7,29 +7,5 @@ export default (store: Object): void => {
     setText: action('setText', (text: string) => {
       store.treeFilter.text = text
     }),
-    id: null,
-    setId: action('setId', (id: string) => {
-      store.treeFilter.id = id
-    }),
-    taxonomyObjectId: computed(
-      () => {
-        const { treeFilter } = store
-        if (treeFilter.id && treeFilter.type === 'taxonomyObject') {
-          return treeFilter.id
-        }
-        return null
-      },
-      { name: 'taxonomyObjectId' }
-    ),
-    propertyCollectionObjectId: computed(
-      () => {
-        const { treeFilter } = store
-        if (treeFilter.id && treeFilter.type === 'propertyCollection') {
-          return treeFilter.id
-        }
-        return null
-      },
-      { name: 'propertyCollectionObjectId' }
-    ),
   })
 }
