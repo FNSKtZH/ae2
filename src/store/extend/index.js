befc6cc6615d15faf047aa8afa3bd50d7de88c6b
Alexander Gabriel <alex.barbalex@gmail.com>
2017-11-19 20:43:17 +0100

    remove unneeded

--- src/store/extend/index.js
+++ src/store/extend/index.js
@@ -1,10 +1,8 @@
 // @flow
 import extendStore from './store'
 import extendExport from './export'
-import extendStoreAutoruns from './storeAutoruns'
 
 export default (store: Object): void => {
   extendStore(store)
   extendExport(store)
-  extendStoreAutoruns(store)
 }
