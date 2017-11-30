51a39b8de40909369a21414fc21a65c560222df4
Alexander Gabriel <alex@gabriel-software.ch>
2017-11-30 12:47:52 +0100

    add taxonomies_of_categories_function

---
+++ src/sql/taxonomies_of_categories.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION ae.taxonomies_of_categories_function()
+  RETURNS setof ae.taxonomies_of_categories AS
+  $$
+    WITH categoryTaxonomies AS (
+      SELECT ae.object.id, ae.category.name AS category_name, ae.taxonomy.name AS taxonomy_name
+      FROM ae.taxonomy
+        INNER JOIN ae.object
+          INNER JOIN ae.category
+          ON ae.category.name = ae.object.category
+        ON ae.object.taxonomy_id = ae.taxonomy.id
+      GROUP BY ae.object.id, ae.category.name, ae.taxonomy.name
+    )
+    SELECT category_name, taxonomy_name, count(*) AS count
+    FROM categoryTaxonomies
+    GROUP BY category_name, taxonomy_name
+    ORDER BY category_name, taxonomy_name
+  $$
+  LANGUAGE sql STABLE;
+ALTER FUNCTION ae.taxonomies_of_categories_function()
+  OWNER TO postgres;
