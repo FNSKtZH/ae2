cd5cf4d86cc33f4dfec678c7f28d9bb3883fd8b3
Alexander Gabriel <alex.barbalex@gmail.com>
2017-12-06 14:33:27 +0100

    optimize

--- src/modules/PgConnectionArgFilterOperatorsPlugin.js
+++ src/modules/PgConnectionArgFilterOperatorsPlugin.js
@@ -1,32 +1,32 @@
 module.exports = function PgConnectionArgFilterOperatorsPlugin(
-    builder,
-    { connectionFilterUsesShortNames = false } = {}
-  ) {
-    builder.hook(
-      "init",
-      (
-        _,
-        {
-          getTypeByName,
-          addConnectionFilterOperator,
-          escapeLikeWildcards,
-          pgSql: sql,
-          graphql: { GraphQLBoolean, GraphQLList, GraphQLNonNull },
-        }
-      ) => {
-        addConnectionFilterOperator(
-          connectionFilterUsesShortNames ? "conti" : "containsInsensitive",
-          "Checks for strings containing this value.  Case insensitive.",
-          typeName => getTypeByName(typeName),
-          (identifier, val) => {
-            return sql.query`${identifier}->>'${val.key}' ILIKE '%${val.val}%'`;
-          },
-          {
-            allowedFieldTypes: ["Json"],
-            inputResolver: input => `%${escapeLikeWildcards(input)}%`,
-          }
-        );
-        return _;
+  builder,
+  { connectionFilterUsesShortNames = false } = {}
+) {
+  builder.hook(
+    'init',
+    (
+      _,
+      {
+        getTypeByName,
+        addConnectionFilterOperator,
+        escapeLikeWildcards,
+        pgSql: sql,
+        graphql: { GraphQLBoolean, GraphQLList, GraphQLNonNull },
       }
-    );
-  };
+    ) => {
+      addConnectionFilterOperator(
+        connectionFilterUsesShortNames ? 'conti' : 'containsInsensitive',
+        'Checks for strings containing this value.  Case insensitive.',
+        typeName => getTypeByName(typeName),
+        (identifier, val) => {
+          return sql.query`${identifier}->>'${val.key}' ILIKE '%${val.val}%'`
+        },
+        {
+          allowedFieldTypes: ['JSON'],
+          inputResolver: input => `%${escapeLikeWildcards(input)}%`,
+        }
+      )
+      return _
+    }
+  )
+}
