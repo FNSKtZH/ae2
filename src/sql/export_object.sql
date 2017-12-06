05c15ff1cd218b1ce01199c2a0323082e2cebfbe
Alexander Gabriel <alex@gabriel-software.ch>
2017-12-06 18:25:31 +0100

    sync

--- src/sql/export_object.sql
+++ src/sql/export_object.sql
@@ -1,6 +1,7 @@
 select
     ae.taxonomy.name as taxonomy_name,
     ae.object.name as object_name,
+    ae.object.properties->>'Gattung' AS regexp_replace(concat_ws('_', ae.taxonomy.name::text, 'Gattung'), '\s', '-'),
     ae.property_collection_object.id as pc_id
 from
     ae.object
@@ -10,6 +11,8 @@ from
         inner join ae.property_collection
         on ae.property_collection_object.property_collection_id = ae.property_collection.id
     on ae.object.id = ae.property_collection_object.object_id
+WHERE
+    ae.object.properties->>'Gattung' ILIKE '%rosa%'
 order by
     taxonomy_name,
     object_name
