2e3255ffb4787c1462023fc95d3f3250a105245c
Alexander Gabriel <alex@gabriel-software.ch>
2017-12-09 00:38:17 +0100

    get export_object function to work

---
+++ src/sql/export_object_function.sql
@@ -0,0 +1,24 @@
+CREATE TYPE tax_filter AS (
+    comparator text,
+    pname text,
+    taxname text,
+    value text
+);
+CREATE OR REPLACE FUNCTION ae.export_object(export_taxonomies text[], tax_filters tax_filter[])
+  RETURNS setof ae.object AS
+  $$
+    DECLARE
+        f tax_filter;
+        sql text := 'SELECT ae.object.* FROM ae.object INNER JOIN ae.taxonomy ON ae.object.taxonomy_id = ae.taxonomy.id WHERE ae.taxonomy.name = ANY ($1)';
+    BEGIN
+        FOREACH f IN ARRAY tax_filters
+        LOOP
+            sql := sql || ' AND ae.object.properties->>' || quote_literal(f.pname) || ' ' || quote_literal(f.comparator) || ' ' || quote_literal(f.value);
+        END LOOP;
+        RAISE LOG 'log: sql created after loop: %', sql;
+    RETURN QUERY EXECUTE sql USING export_taxonomies, tax_filters;
+    END
+  $$
+  LANGUAGE plpgsql STABLE;
+ALTER FUNCTION ae.export_object(export_taxonomies text[], tax_filters tax_filter[])
+  OWNER TO postgres;
