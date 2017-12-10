7835b2c98c9414c30e499b6682a6a6e705ef8527
Alexander Gabriel <alex.barbalex@gmail.com>
2017-12-10 21:01:47 +0100

    work on export pco function

--- src/sql/export_object_function.sql
+++ src/sql/export_object_function.sql
@@ -8,12 +8,12 @@ CREATE OR REPLACE FUNCTION ae.export_object(export_taxonomies text[], tax_filter
   RETURNS setof ae.object AS
   $$
     DECLARE
-        f tax_filter;
+        tf tax_filter;
         sql text := 'SELECT ae.object.* FROM ae.object INNER JOIN ae.taxonomy ON ae.object.taxonomy_id = ae.taxonomy.id WHERE ae.taxonomy.name = ANY($1)';
     BEGIN
-        FOREACH f IN ARRAY tax_filters
+        FOREACH tf IN ARRAY tax_filters
         LOOP
-            sql := sql || ' AND ae.object.properties->>' || quote_literal(f.pname) || ' ' || f.comparator || ' ' || quote_literal(f.value);
+            sql := sql || ' AND ae.object.properties->>' || quote_literal(tf.pname) || ' ' || tf.comparator || ' ' || quote_literal(tf.value);
         END LOOP;
     RETURN QUERY EXECUTE sql USING export_taxonomies, tax_filters;
     END
--- src/sql/export_pco_function.sql
+++ src/sql/export_pco_function.sql
@@ -1,11 +1,11 @@
 CREATE TYPE pco_filter AS (
     comparator text,
     pname text,
-    pcnname text,
+    pcname text,
     value text
 );
-CREATE OR REPLACE FUNCTION ae.export_object(export_taxonomies text[], tax_filters tax_filter[], pco_filters pco_filter[])
-  RETURNS setof ae.object AS
+CREATE OR REPLACE FUNCTION ae.export_pco(export_taxonomies text[], tax_filters tax_filter[], pco_filters pco_filter[])
+  RETURNS setof ae.property_collection_object AS
   $$
     DECLARE
         tf tax_filter;
@@ -40,5 +40,5 @@ CREATE OR REPLACE FUNCTION ae.export_object(export_taxonomies text[], tax_filter
     END
   $$
   LANGUAGE plpgsql STABLE;
-ALTER FUNCTION ae.export_object(export_taxonomies text[], tax_filters tax_filter[], pco_filters pco_filter[])
+ALTER FUNCTION ae.export_pco(export_taxonomies text[], tax_filters tax_filter[], pco_filters pco_filter[])
   OWNER TO postgres;
