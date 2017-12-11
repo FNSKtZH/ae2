78c0c29e757c4ac4b06abfe53575dd7430c5599a
Alexander Gabriel <alex@gabriel-software.ch>
2017-12-11 14:55:51 +0100

    work on fetching export data

--- src/modules/exportDataGql.js
+++ src/modules/exportDataGql.js
@@ -4,16 +4,73 @@ import gql from 'graphql-tag'
 
 export default gql`
   query exportDataQuery(
-    $existsLevel2Pc: Boolean!
-    $notExistsLevel2Pc: Boolean!
-    $existsLevel2Taxonomy: Boolean!
-    $existsLevel3: Boolean!
-    $level3Taxonomy: String!
-    $existsLevel4: Boolean!
-    $level4Taxonomy: UUID!
+    $exportTaxonomies: Array!
+    $taxFilters: Array!
+    $pcoFilters: Array!
+    $rcoFilters: Array!
   ) {
-    allPropertyCollections @include(if: $notExistsLevel2Pc) {
+    exportObject(
+      exportTaxonomies: $exportTaxonomies
+      taxFilters: $taxFilters
+      pcoFilters: $pcoFilters
+      rcoFilters: $rcoFilters
+    ) {
       totalCount
+      nodes {
+        id
+        name
+        properties
+      }
+    }
+    exportPco(
+      exportTaxonomies: $exportTaxonomies
+      taxFilters: $taxFilters
+      pcoFilters: $pcoFilters
+    ) {
+      totalCount
+      nodes {
+        id
+        objectId
+        properties
+      }
+    }
+    exportSynonymPco(
+      exportTaxonomies: $exportTaxonomies
+      taxFilters: $taxFilters
+      pcoFilters: $pcoFilters
+    ) {
+      totalCount
+      nodes {
+        id
+        objectId
+        properties
+      }
+    }
+    exportRco(
+      exportTaxonomies: $exportTaxonomies
+      taxFilters: $taxFilters
+      rcoFilters: $rcoFilters
+    ) {
+      totalCount
+      nodes {
+        id
+        objectId
+        relationType
+        properties
+      }
+    }
+    exportSynonymRco(
+      exportTaxonomies: $exportTaxonomies
+      taxFilters: $taxFilters
+      rcoFilters: $rcoFilters
+    ) {
+      totalCount
+      nodes {
+        id
+        objectId
+        relationType
+        properties
+      }
     }
   }
 `
