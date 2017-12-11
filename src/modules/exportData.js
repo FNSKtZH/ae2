836b4ecbf2ec56f77ae7686758df487a3312e03d
Alexander Gabriel <alex.barbalex@gmail.com>
2017-12-11 15:11:20 +0100

    work on export preview

---
+++ src/modules/exportData.js
@@ -0,0 +1,27 @@
+// @flow
+import { graphql } from "react-apollo";
+import get from "lodash/get";
+
+import exportDataGql from "./exportDataGql";
+
+export default graphql(exportDataGql, {
+  options: ({
+    exportTaxonomiesData,
+    exportTaxFiltersData,
+    exportPcoFiltersData,
+    exportRcoFiltersData
+  }: {
+    exportTaxonomiesData: Object,
+    exportTaxFiltersData: Object,
+    exportPcoFiltersData: Object,
+    exportRcoFiltersData: Object
+  }) => ({
+    variables: {
+      exportTaxonomies: get(exportTaxonomiesData, "exportTaxonomies", []),
+      taxFilters: get(exportTaxFiltersData, "exportTaxFilters", []),
+      pcoFilters: get(exportPcoFiltersData, "exportPcoFilters", []),
+      rcoFilters: get(exportRcoFiltersData, "exportRcoFilters", [])
+    }
+  }),
+  name: "treeData"
+});
