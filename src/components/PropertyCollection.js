d2c7dd29ce6284ae19f87ef05f79b5d7460d3707
Alexander Gabriel <alex.barbalex@gmail.com>
2017-12-14 22:07:25 +0100

    post idea for pc

--- src/components/PropertyCollection.js
+++ src/components/PropertyCollection.js
@@ -1,6 +1,24 @@
 // @flow
 import React from 'react'
+import styled from 'styled-components'
 
-const PropertyCollection = () => <div>EigenschaftenSammlung</div>
+const Container = styled.div`
+  padding: 10px;
+`
+
+const PropertyCollection = () => (
+  <Container>
+    <h3>Eigenschaften-Sammlung</h3>
+    <div>Idee:</div>
+    <ul>
+      <li>Hier erscheint die Beschreibung der Eigenschaften-Sammlung</li>
+      <li>
+        Im Baum eine Ebene tiefer, werden die Eigenschaften in einer Tabelle
+        angezeigt. Exportierbar
+      </li>
+      <li>Im Baum darunter, werden allfällige Beziehungen angezeigt</li>
+    </ul>
+  </Container>
+)
 
 export default PropertyCollection
