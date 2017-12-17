d4deb1df353d427723542b3ac2515fcf4de1e611
Alexander Gabriel <alex@gabriel-software.ch>
2017-12-17 22:02:52 +0100

    move from ae_import

---
+++ src/sql/createDatabase.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE ae encoding 'UTF8';
+CREATE SCHEMA ae;
+CREATE SCHEMA auth;
+CREATE EXTENSION "uuid-ossp";
+DROP ROLE IF EXISTS org_collection_writer;
+CREATE ROLE org_collection_writer;
+DROP ROLE IF EXISTS org_habitat_writer;
+CREATE ROLE org_habitat_writer;
+DROP ROLE IF EXISTS org_taxonomy_writer;
+CREATE ROLE org_taxonomy_writer;
+DROP ROLE IF EXISTS org_admin;
+CREATE ROLE org_admin;
