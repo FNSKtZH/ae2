d8c9ac388a9a69fe94d2bf09990ec54699b7e20f
Alexander Gabriel <alex.barbalex@gmail.com>
2017-12-18 10:30:48 +0100

    prepare showing pc data

--- src/components/PropertyCollection.js
+++ src/components/PropertyCollection.js
@@ -1,24 +1,38 @@
 // @flow
 import React from 'react'
+import compose from 'recompose/compose'
 import styled from 'styled-components'
+
+import activeNodeArrayData from '../modules/activeNodeArrayData'
+
+const enhance = compose(activeNodeArrayData)
 
 const Container = styled.div`
   padding: 10px;
 `
 
-const PropertyCollection = () => (
-  <Container>
-    <h3>Eigenschaften-Sammlung</h3>
-    <div>Idee:</div>
-    <ul>
-      <li>Hier erscheint die Beschreibung der Eigenschaften-Sammlung</li>
-      <li>
-        Im Baum eine Ebene tiefer, werden die Eigenschaften in einer Tabelle
-        angezeigt. Exportierbar
-      </li>
-      <li>Im Baum darunter, werden allfällige Beziehungen angezeigt</li>
-    </ul>
-  </Container>
-)
+const PropertyCollection = ({
+  activeNodeArrayData,
+}: {
+  activeNodeArrayData: Object,
+}) => {
+  const { activeNodeArray } = activeNodeArrayData
+  const pcId = activeNodeArray[1]
 
-export default PropertyCollection
+  return (
+    <Container>
+      <h3>Eigenschaften-Sammlung</h3>
+      <div>Idee:</div>
+      <ul>
+        <li>Hier erscheint die Beschreibung der Eigenschaften-Sammlung</li>
+        <li>
+          Im Baum eine Ebene tiefer, werden die Eigenschaften in einer Tabelle
+          angezeigt. Exportierbar
+        </li>
+        <li>Im Baum darunter, werden allfällige Beziehungen angezeigt</li>
+      </ul>
+    </Container>
+  )
+}
+
+export default enhance(PropertyCollection)
