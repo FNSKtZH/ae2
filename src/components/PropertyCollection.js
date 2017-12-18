16b555e10b05a12fde221e419ad42072335888ad
Alexander Gabriel <alex.barbalex@gmail.com>
2017-12-18 12:37:56 +0100

    add pc properties

--- src/components/PropertyCollection.js
+++ src/components/PropertyCollection.js
@@ -3,9 +3,11 @@ import React from 'react'
 import compose from 'recompose/compose'
 import styled from 'styled-components'
 import get from 'lodash/get'
+import format from 'date-fns/format'
 
 import activeNodeArrayData from '../modules/activeNodeArrayData'
 import pCData from '../modules/pCData'
+import PropertyReadOnly from './PropertyReadOnly'
 
 const enhance = compose(activeNodeArrayData, pCData)
 
@@ -15,8 +17,8 @@ const Container = styled.div`
 
 const PropertyCollection = ({ pCData }: { pCData: Object }) => {
   const pC = get(pCData, 'propertyCollectionById', {})
-  console.log('pCData:', pCData)
   console.log('pC:', pC)
+  const org = get(pC, ('organizationByOrganizationId.name': ''))
 
   return (
     <Container>
@@ -30,6 +32,35 @@ const PropertyCollection = ({ pCData }: { pCData: Object }) => {
         </li>
         <li>Im Baum darunter, werden allfällige Beziehungen angezeigt</li>
       </ul>
+      <PropertyReadOnly key="name" value={pC.name} label="Name" />
+      <PropertyReadOnly
+        key="description"
+        value={pC.description}
+        label="Beschreibung"
+      />
+      <PropertyReadOnly
+        key="combining"
+        value={
+          pC.combining !== undefined
+            ? pC.combining
+                .toString()
+                .replace('true', 'ja')
+                .replace('false', 'nein')
+            : ''
+        }
+        label="zusammenfassend"
+      />
+      <PropertyReadOnly
+        key="lastUpdated"
+        value={format(new Date(pC.lastUpdated), 'DD.MM.YYYY')}
+        label="Zuletzt aktualisiert"
+      />
+      <PropertyReadOnly
+        key="links"
+        value={pC.links ? pC.links.join(', ') : ''}
+        label="Links"
+      />
+      <PropertyReadOnly key="org" value={org} label="Zuständige Organisation" />
     </Container>
   )
 }
