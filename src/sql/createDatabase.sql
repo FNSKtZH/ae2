858f6e3e36e69787eb8dc21689096432adc10d65
Alexander Gabriel <alex@gabriel-software.ch>
2017-12-19 14:07:32 +0100

    add all stuff for first creation

--- src/sql/createDatabase.sql
+++ src/sql/createDatabase.sql
@@ -1,5 +1,10 @@
 CREATE DATABASE ae encoding 'UTF8';
-CREATE SCHEMA ae;
-CREATE SCHEMA auth;
+CREATE SCHEMA IF NOT EXISTS ae;
+-- We put things inside the auth schema to hide
+-- them from public view. Certain public procs/views will
+-- refer to helpers and tables inside.
+CREATE SCHEMA IF NOT EXISTS auth;
 CREATE EXTENSION "uuid-ossp";
 CREATE EXTENSION pgcrypto;
+-- run this once with real secret
+ALTER DATABASE ae SET "app.jwt_secret" TO 'secret';
