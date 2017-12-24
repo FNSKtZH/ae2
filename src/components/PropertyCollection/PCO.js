043cc802fc815b645c0dc5ba76e2ab584d07671b
Alexander Gabriel <alex.barbalex@gmail.com>
2017-12-24 16:39:29 +0100

    format and add numbers

--- src/components/PropertyCollection/PCO.js
+++ src/components/PropertyCollection/PCO.js
@@ -12,29 +12,52 @@ import pCOData from './pCOData'
 
 const enhance = compose(activeNodeArrayData, pCOData)
 
-const Container = styled.div `
+const Container = styled.div`
   padding: 10px;
+  .react-grid-Container {
+    font-size: small;
+  }
+  .react-grid-Header {
+  }
+  .react-grid-HeaderRow {
+    overflow: hidden;
+  }
+  .react-grid-HeaderCell:not(:first-child) {
+    border-left: #c7c7c7 solid 1px !important;
+  }
+  .react-grid-HeaderCell__draggable {
+    right: 16px !important;
+  }
+  .react-grid-Cell {
+    border: #ddd solid 1px !important;
+  }
+`
+const SpreadsheetContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const TotalDiv = styled.div`
+  font-size: small;
+  padding-left: 9px;
+  margin-top: 4px;
 `
 
-const PropertyCollection = ({
-  pCOData
-}: {
-  pCOData: Object
-}) => {
+const PropertyCollection = ({ pCOData }: { pCOData: Object }) => {
   const pCO = []
   const pCORaw = get(
     pCOData,
-    'propertyCollectionById.propertyCollectionObjectsByPropertyCollectionId.nodes', []
+    'propertyCollectionById.propertyCollectionObjectsByPropertyCollectionId.nodes',
+    []
   ).map(p => omit(p, ['__typename']))
   pCORaw.forEach(p => {
     let nP = {
       objectId: p.objectId,
-      objectName: get(p, 'objectByObjectId.name', null)
+      objectName: get(p, 'objectByObjectId.name', null),
     }
     if (p.properties) {
       const props = JSON.parse(p.properties)
       forOwn(props, (value, key) => {
-        if (typeof (value) === 'boolean') {
+        if (typeof value === 'boolean') {
           nP[key] = value.toString()
         } else {
           nP[key] = value
@@ -45,44 +68,37 @@ const PropertyCollection = ({
   })
   //console.log('PCO: pCORaw:', pCORaw)
   //console.log('PCO: pCO:', pCO)
-  const columns = pCO[0] ?
-    Object.keys(pCO[0]).map(k => ({
-      key: k,
-      name: k,
-      resizable: true,
-      sortable: true,
-    })) : []
+  const columns = pCO[0]
+    ? Object.keys(pCO[0]).map(k => ({
+        key: k,
+        name: k,
+        resizable: true,
+        sortable: true,
+      }))
+    : []
   //console.log('PCO: columns:', columns)
   // TODO: map pCO and extract all properties, meanwhile building columns
 
-  return ( <
-    Container > {
-      pCO.length > 0 && ( <
-        ReactDataGrid onGridSort = {
-          (column, direction) => {
-            //setSortField(column)
-            //setSortDirection(direction.toLowerCase())
-          }
-        }
-        columns = {
-          columns
-        }
-        rowGetter = {
-          i => pCO[i]
-        }
-        rowsCount = {
-          pCO.length
-        }
-        minHeight = {
-          500
-        }
-        minColumnWidth = {
-          120
-        }
-        />
-      )
-    } <
-    /Container>
+  return (
+    <Container>
+      <SpreadsheetContainer>
+        {pCO.length > 0 && (
+          <ReactDataGrid
+            onGridSort={(column, direction) => {
+              //setSortField(column)
+              //setSortDirection(direction.toLowerCase())
+            }}
+            columns={columns}
+            rowGetter={i => pCO[i]}
+            rowsCount={pCO.length}
+            minHeight={500}
+            minColumnWidth={120}
+          />
+        )}
+        <TotalDiv>{`${pCO.length} Datensätze, ${columns.length -
+          2} Felder`}</TotalDiv>
+      </SpreadsheetContainer>
+    </Container>
   )
 }
 
