df79ac75c635ae8fdee1ffbdfcff8060f2df7538
Alexander Gabriel <alex.barbalex@gmail.com>
2017-12-27 21:14:39 +0100

    uri-decode uri elements

--- src/modules/getActiveNodeArrayFromPathname.js
+++ src/modules/getActiveNodeArrayFromPathname.js
@@ -1,13 +1,15 @@
 // @flow
 export default (): Array<number | string> => {
   const pathName = window.location.pathname.replace('/', '')
-  const pathElements = pathName.split('/')
+  let pathElements = pathName.split('/')
   if (pathElements[0] === '') {
     // get rid of empty element(s) at start
     pathElements.shift()
   }
+  // decode elements
+  pathElements = pathElements.map(e => decodeURIComponent(e))
   // convert numbers to numbers
-  // //stackoverflow.com/questions/175739/is-there-a-built-in-way-in-javascript-to-check-if-a-string-is-a-valid-number
+  //stackoverflow.com/questions/175739/is-there-a-built-in-way-in-javascript-to-check-if-a-string-is-a-valid-number
   pathElements.forEach((e, index) => {
     if (!isNaN(e)) {
       pathElements[index] = +e
