f36979974df2970ac882da06fffa212f9ad744ed
Alexander Gabriel <alex.barbalex@gmail.com>
2017-12-27 14:22:50 +0100

    style

--- src/components/Login/User/PCs.js
+++ src/components/Login/User/PCs.js
@@ -3,22 +3,35 @@ import React from 'react'
 import get from 'lodash/get'
 import styled from 'styled-components'
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 const Label = styled.div`
   color: rgba(0, 0, 0, 0.3);
   margin-bottom: 10px;
   margin-top: 10px;
 `
+const List = styled.div`
+  column-width: 400px;
+  margin-bottom: 10px;
+  ul {
+    -webkit-margin-before: 0px;
+  }
+`
 
 const PCs = ({ pcs }: { pcs: Array<Object> }) => (
-  <div>
+  <Container>
     <Label>Importierte Eigenschaftensammlungen:</Label>
-    <ul>
-      {pcs.map(u => {
-        const name = get(u, 'name', '')
-        return <li key={name}>{name}</li>
-      })}
-    </ul>
-  </div>
+    <List>
+      <ul>
+        {pcs.map(u => {
+          const name = get(u, 'name', '')
+          return <li key={name}>{name}</li>
+        })}
+      </ul>
+    </List>
+  </Container>
 )
 
 export default PCs
--- src/components/Login/User/Roles.js
+++ src/components/Login/User/Roles.js
@@ -3,24 +3,37 @@ import React from 'react'
 import get from 'lodash/get'
 import styled from 'styled-components'
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 const Label = styled.div`
   color: rgba(0, 0, 0, 0.3);
   margin-bottom: 10px;
   margin-top: 10px;
 `
+const List = styled.div`
+  column-width: 400px;
+  margin-bottom: 10px;
+  ul {
+    -webkit-margin-before: 0px;
+  }
+`
 
 const Roles = ({ orgUsers }: { orgUsers: Array<Object> }) => (
-  <div>
+  <Container>
     <Label>Rollen:</Label>
-    <ul>
-      {orgUsers.map(u => {
-        const orgName = get(u, 'organizationByOrganizationId.name', '')
-        const role = get(u, 'role', '')
-        const val = `${orgName}: ${role}`
-        return <li key={val}>{val}</li>
-      })}
-    </ul>
-  </div>
+    <List>
+      <ul>
+        {orgUsers.map(u => {
+          const orgName = get(u, 'organizationByOrganizationId.name', '')
+          const role = get(u, 'role', '')
+          const val = `${orgName}: ${role}`
+          return <li key={val}>{val}</li>
+        })}
+      </ul>
+    </List>
+  </Container>
 )
 
 export default Roles
