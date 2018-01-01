a0c10d3ba59ba2a5d50bc14a503c23676f9f6967
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-01 11:13:10 +0100

    optimize

--- src/components/Benutzer/index.js
+++ src/components/Benutzer/index.js
@@ -1,10 +1,14 @@
 // @flow
-import React, { Component } from 'react'
+import React, {
+  Component
+} from 'react'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
 import styled from 'styled-components'
 import compose from 'recompose/compose'
-import { withApollo } from 'react-apollo'
+import {
+  withApollo
+} from 'react-apollo'
 import app from 'ampersand-app'
 import get from 'lodash/get'
 
@@ -16,39 +20,46 @@ import Roles from './Roles'
 import PCs from './PCs'
 import onSave from './onSave'
 
-const Container = styled.div`
+const Container = styled.div `
   padding: 10px;
 `
-const SaveButton = styled(RaisedButton)`
+const SaveButton = styled(RaisedButton)
+`
   margin-bottom: 15px;
 `
 
-const enhance = compose(withApollo,activeNodeArrayData, loginData, userData)
+const enhance = compose(withApollo, activeNodeArrayData, loginData, userData)
 
-class User extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: undefined,
-      nameErrorText: null,
-      email: undefined,
-      pass: '',
-      passErrorText: 'Bitte Passwort eingeben, um Änderungen zu speichern',
-      passNew: '',
-    }
-  }
+type State = {
+  name: string,
+  nameErrorText: string,
+  email: string,
+  pass: string,
+  passErrorText: string,
+  passNew: string
+}
 
-  props: {
-    client: Object,
-    loginData: Object,
-    userData: Object,
+type Props = {
+  client: Object,
+  loginData: Object,
+  userData: Object
+}
+
+class User extends Component < Props, State > {
+  state = {
+    name: '',
+    nameErrorText: '',
+    email: '',
+    pass: '',
+    passErrorText: 'Bitte Passwort eingeben, um Änderungen zu speichern',
+    passNew: '',
   }
 
   componentDidUpdate(prevProps, prevState) {
     const propsUser = get(this.props.userData, 'userByName', {})
     const prevPropsUser = get(prevProps.userData, 'userByName', {})
 
-    if (!!propsUser.id && prevPropsUser.id === undefined) {
+    if (!!propsUser && !!propsUser.id && prevPropsUser && prevPropsUser.id === undefined) {
       this.setState({
         name: propsUser.name,
         email: propsUser.email,
@@ -63,7 +74,9 @@ class User extends Component {
   }
 
   onLogout = () => {
-    const { client } = this.props
+    const {
+      client
+    } = this.props
     app.idb.users.clear()
     client.mutate({
       mutation: setLoginMutation,
@@ -79,10 +92,13 @@ class User extends Component {
     const props = this.props
     const state = this.state
 
-    onSave({ props, state }).then(() => {
+    onSave({
+      props,
+      state
+    }).then(() => {
       this.setState({
-        nameErrorText: null,
-        passErrorText: null,
+        nameErrorText: '',
+        passErrorText: '',
         pass: '',
       })
       // need to refetch so pass disappears
@@ -91,7 +107,9 @@ class User extends Component {
   }
 
   render() {
-    const { userData } = this.props
+    const {
+      userData
+    } = this.props
     const {
       name,
       nameErrorText,
@@ -103,60 +121,96 @@ class User extends Component {
     const user = get(userData, 'userByName', {})
     const orgUsers = get(user, 'organizationUsersByUserId.nodes', [])
     const pcs = get(user, 'propertyCollectionsByImportedBy.nodes', [])
-    const showPass =
-      !!name &&
+    const showPass = !!name &&
       !!user.name &&
       !!email &&
       !!user.email &&
       (name !== user.name || email !== user.email)
     const saveEnabled = !!pass && showPass
 
-    return (
-      <Container>
-        <RaisedButton label="abmelden" onClick={this.onLogout} />
-        <TextField
-          name="name"
-          floatingLabelText="Name"
-          errorText={nameErrorText}
-          value={name}
-          onChange={this.onChangeVal}
-          fullWidth
-        />
-        <TextField
-          name="email"
-          floatingLabelText="Email"
-          value={email}
-          onChange={this.onChangeVal}
-          fullWidth
-        />
-        <TextField
-          name="passNew"
-          floatingLabelText="Passwort ändern"
-          type="password"
-          value={passNew}
-          onChange={this.onChangeVal}
-          fullWidth
-        />
-        {showPass && (
-          <TextField
-            name="pass"
-            floatingLabelText="Passwort (aktuell)"
-            errorText={passErrorText}
-            errorStyle={{ color: 'green' }}
-            type="password"
-            value={pass}
-            onChange={this.onChangeVal}
-            fullWidth
-          />
-        )}
-        <SaveButton
-          label="Änderungen speichern"
-          onClick={this.onSave}
-          disabled={!saveEnabled}
-        />
-        {orgUsers.length > 0 && <Roles orgUsers={orgUsers} />}
-        {pcs.length > 0 && <PCs pcs={pcs} />}
-      </Container>
+    return ( <
+      Container >
+      <
+      RaisedButton label = "abmelden"
+      onClick = {
+        this.onLogout
+      }
+      /> <
+      TextField name = "name"
+      floatingLabelText = "Name"
+      errorText = {
+        nameErrorText
+      }
+      value = {
+        name
+      }
+      onChange = {
+        this.onChangeVal
+      }
+      fullWidth /
+      >
+      <
+      TextField name = "email"
+      floatingLabelText = "Email"
+      value = {
+        email
+      }
+      onChange = {
+        this.onChangeVal
+      }
+      fullWidth /
+      >
+      <
+      TextField name = "passNew"
+      floatingLabelText = "Passwort ändern"
+      type = "password"
+      value = {
+        passNew
+      }
+      onChange = {
+        this.onChangeVal
+      }
+      fullWidth /
+      >
+      {
+        showPass && ( <
+          TextField name = "pass"
+          floatingLabelText = "Passwort (aktuell)"
+          errorText = {
+            passErrorText
+          }
+          errorStyle = {
+            {
+              color: 'green'
+            }
+          }
+          type = "password"
+          value = {
+            pass
+          }
+          onChange = {
+            this.onChangeVal
+          }
+          fullWidth /
+          >
+        )
+      } <
+      SaveButton label = "Änderungen speichern"
+      onClick = {
+        this.onSave
+      }
+      disabled = {!saveEnabled
+      }
+      /> {
+      orgUsers.length > 0 && < Roles orgUsers = {
+        orgUsers
+      }
+      />} {
+      pcs.length > 0 && < PCs pcs = {
+        pcs
+      }
+      />} < /
+      Container >
     )
   }
 }
