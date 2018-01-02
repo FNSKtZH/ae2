2fd0b3fb37723e18420116600ec733ae00795cc4
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-02 13:52:33 +0100

    add function

---
+++ src/sql/categories_of_taxonomies_function.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION ae.categories_of_taxonomies_function()
+  RETURNS TABLE (
+    taxonomy_id UUID,
+    taxonomy_name TEXT,
+    category_name TEXT
+  ) AS
+  $$
+    SELECT DISTINCT
+      ae.taxonomy.id as taxonomy_id,
+      ae.taxonomy.name as taxonomy_name,
+      ae.category.name as category_name
+    FROM ae.taxonomy
+      INNER JOIN ae.object
+        INNER JOIN ae.category
+        ON ae.category.name = ae.object.category
+      ON ae.object.taxonomy_id = ae.taxonomy.id
+    ORDER BY
+      taxonomy_name,
+      category_name
+  $$
+  LANGUAGE sql STABLE;
+ALTER FUNCTION ae.categories_of_taxonomies_function()
+  OWNER TO postgres;
