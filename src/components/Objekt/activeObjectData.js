283c420c67ba5219e252203377a1a3fb2443977a
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-17 08:40:06 +0100

    simplify

--- src/components/Objekt/activeObjectData.js
+++ src/components/Objekt/activeObjectData.js
@@ -1,16 +1,232 @@
 // @flow
 import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
-import activeObectGql from './activeObjectGql'
 import getActiveObjectIdFromNodeArray from '../../modules/getActiveObjectIdFromNodeArray'
 
-export default graphql(activeObectGql, {
-  options: ({ activeNodeArrayData }) => ({
-    variables: {
-      activeObjectId: getActiveObjectIdFromNodeArray(
-        activeNodeArrayData.activeNodeArray
-      ),
-    },
-  }),
-  name: 'activeObjectData',
-})
+export default graphql(
+  gql`
+    query ObjectQuery($activeObjectId: UUID!) {
+      objectById(id: $activeObjectId) {
+        id
+        taxonomyId
+        parentId
+        name
+        properties
+        category
+        idOld
+        synonymsByObjectId {
+          totalCount
+          nodes {
+            objectId
+            objectIdSynonym
+            objectByObjectIdSynonym {
+              id
+              taxonomyId
+              parentId
+              name
+              properties
+              category
+              idOld
+              taxonomyByTaxonomyId {
+                id
+                name
+                description
+                links
+                lastUpdated
+                isCategoryStandard
+                importedBy
+                termsOfUse
+                habitatLabel
+                habitatComments
+                habitatNrFnsMin
+                habitatNrFnsMax
+                organizationByOrganizationId {
+                  id
+                  name
+                }
+              }
+              propertyCollectionObjectsByObjectId {
+                totalCount
+                nodes {
+                  id
+                  objectId
+                  propertyCollectionId
+                  properties
+                  propertyCollectionByPropertyCollectionId {
+                    id
+                    name
+                    description
+                    links
+                    combining
+                    lastUpdated
+                    termsOfUse
+                    importedBy
+                    organizationByOrganizationId {
+                      id
+                      name
+                    }
+                    userByImportedBy {
+                      id
+                      name
+                      email
+                    }
+                  }
+                }
+              }
+              relationsByObjectId {
+                totalCount
+                nodes {
+                  id
+                  propertyCollectionId
+                  objectId
+                  objectIdRelation
+                  relationType
+                  properties
+                  propertyCollectionByPropertyCollectionId {
+                    id
+                    name
+                    description
+                    links
+                    combining
+                    lastUpdated
+                    termsOfUse
+                    importedBy
+                    organizationByOrganizationId {
+                      id
+                      name
+                    }
+                    userByImportedBy {
+                      id
+                      name
+                      email
+                    }
+                  }
+                  objectByObjectIdRelation {
+                    id
+                    name
+                    category
+                  }
+                }
+              }
+              categoryByCategory {
+                id
+                name
+                dataType
+              }
+              objectByParentId {
+                id
+                objectByParentId {
+                  id
+                  objectByParentId {
+                    id
+                    objectByParentId {
+                      id
+                      objectByParentId {
+                        id
+                        objectByParentId {
+                          id
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        taxonomyByTaxonomyId {
+          id
+          name
+          description
+          links
+          lastUpdated
+          isCategoryStandard
+          importedBy
+          termsOfUse
+          habitatLabel
+          habitatComments
+          habitatNrFnsMin
+          habitatNrFnsMax
+          organizationByOrganizationId {
+            id
+            name
+          }
+        }
+        propertyCollectionObjectsByObjectId {
+          totalCount
+          nodes {
+            id
+            objectId
+            propertyCollectionId
+            properties
+            propertyCollectionByPropertyCollectionId {
+              id
+              name
+              description
+              links
+              combining
+              lastUpdated
+              termsOfUse
+              importedBy
+              organizationByOrganizationId {
+                id
+                name
+              }
+              userByImportedBy {
+                id
+                name
+                email
+              }
+            }
+          }
+        }
+        relationsByObjectId {
+          totalCount
+          nodes {
+            id
+            propertyCollectionId
+            objectId
+            objectIdRelation
+            relationType
+            properties
+            propertyCollectionByPropertyCollectionId {
+              id
+              name
+              description
+              links
+              combining
+              lastUpdated
+              termsOfUse
+              importedBy
+              organizationByOrganizationId {
+                id
+                name
+              }
+              userByImportedBy {
+                id
+                name
+                email
+              }
+            }
+            objectByObjectIdRelation {
+              id
+              name
+              category
+            }
+          }
+        }
+      }
+    }
+  `,
+  {
+    options: ({ activeNodeArrayData }) => ({
+      variables: {
+        activeObjectId: getActiveObjectIdFromNodeArray(
+          activeNodeArrayData.activeNodeArray
+        ),
+      },
+    }),
+    name: 'activeObjectData',
+  }
+)
