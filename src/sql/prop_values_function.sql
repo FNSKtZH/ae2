d2afa6ed37a7faeb08fbd7eff55c7df8d2e6e437
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-17 17:41:05 +0100

    pass pc too

--- src/sql/prop_values_function.sql
+++ src/sql/prop_values_function.sql
@@ -1,19 +1,20 @@
-SELECT distinct ae.property_collection_object.properties->'Artwert' AS value
+SELECT distinct properties->'Artwert' AS value
 FROM ae.property_collection_object
+WHERE property_collection_id = 'c596a521-696f-11e7-b712-f90bfedd3cfd'
 ORDER BY value
 
 
-CREATE OR REPLACE FUNCTION ae.prop_values_function(table_name text, prop text)
+CREATE OR REPLACE FUNCTION ae.prop_values_function(table_name text, prop_name text, pcfield_name text, pc_id uuid)
   RETURNS setof ae.prop_value AS
   $$
     DECLARE
       --sql text := 'SELECT distinct ae.$1.properties->$2 AS value FROM ae.$1 ORDER BY value';
-      sql text := 'SELECT distinct ae.' || $1 || '.properties->' || quote_literal($2) || ' AS value FROM ae.' || $1 || ' ORDER BY value';
+      sql text := 'SELECT DISTINCT properties->' || quote_literal(prop_name) || ' AS value FROM ae.' || table_name || ' WHERE ' || pcfield_name || ' = '  || quote_literal(pc_id) || ' ORDER BY value';
     BEGIN
-      --RAISE EXCEPTION  'sql: %', sql;
-      RETURN QUERY EXECUTE sql USING table_name, prop;
+      RAISE EXCEPTION  'sql: %', sql;
+      RETURN QUERY EXECUTE sql;
     END
   $$
   LANGUAGE plpgsql STABLE;
-ALTER FUNCTION ae.prop_values_function(table_name text, prop text)
+ALTER FUNCTION ae.prop_values_function(table_name text, prop_name text, pcfield_name text, pc_id uuid)
   OWNER TO postgres;
