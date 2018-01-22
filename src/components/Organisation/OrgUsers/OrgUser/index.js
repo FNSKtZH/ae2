a13d6150d376f783b16fb18c91057f54e2e27dfc
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-22 15:52:00 +0100

    modularize OrgUser

--- src/components/Organisation/OrgUsers/OrgUser/index.js
+++ src/components/Organisation/OrgUsers/OrgUser/index.js
@@ -3,52 +3,37 @@ import React from 'react'
 import compose from 'recompose/compose'
 import styled from 'styled-components'
 import get from 'lodash/get'
-import sortBy from 'lodash/sortBy'
 import { withApollo } from 'react-apollo'
 import IconButton from 'material-ui/IconButton'
 import ContentClear from 'material-ui/svg-icons/content/clear'
-import ContentAdd from 'material-ui/svg-icons/content/add'
 import { red500 } from 'material-ui/styles/colors'
 
-import activeNodeArrayData from '../../../modules/activeNodeArrayData'
-import orgUsersData from './orgUsersData'
-import AutocompleteFromArray from '../../shared/AutocompleteFromArray'
-import updateOrgUserMutation from './updateOrgUserMutation'
-import deleteOrgUserMutation from './deleteOrgUserMutation'
-import createOrgUserMutation from './createOrgUserMutation'
-import ErrorBoundary from '../../shared/ErrorBoundary'
+import activeNodeArrayData from '../../../../modules/activeNodeArrayData'
+import orgUsersData from '../orgUsersData'
+import AutocompleteFromArray from '../../../shared/AutocompleteFromArray'
+import updateOrgUserMutation from '../updateOrgUserMutation'
+import deleteOrgUserMutation from '../deleteOrgUserMutation'
+import ErrorBoundary from '../../../shared/ErrorBoundary'
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-left: 10px;
-  padding-right: 10px;
-`
-const List = styled.div`
-  display: flex;
-  flex-direction: column;
-`
 const OrgUserDiv = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
   align-items: flex-end;
 `
-const DeleteButton = styled(IconButton)`
-  top: 10px !important;
-`
 
 const enhance = compose(withApollo, activeNodeArrayData, orgUsersData)
 
 const OrgUsers = ({
   user,
-  users,
+  orgUsersData,
   client,
 }: {
   user: Object,
-  users: Array<Object>,
+  orgUsersData: Object,
   client: Object,
 }) => {
+  const users = get(orgUsersData, 'allUsers.nodes', [])
   const userNames = users.map(u => u.name).sort()
   const roles = get(orgUsersData, 'allRoles.nodes', [])
     .map(u => u.name)
@@ -61,20 +46,20 @@ const OrgUsers = ({
 
   return (
     <ErrorBoundary>
-      <OrgUserDiv key={`${get(u, 'userByUserId.id')}/${u.role}`}>
+      <OrgUserDiv>
         <AutocompleteFromArray
           label="Benutzer"
-          valueText={get(u, 'userByUserId.name')}
+          valueText={get(user, 'userByUserId.name')}
           dataSource={userNames}
           updatePropertyInDb={val => {
             const userId = users.find(u => u.name === val).id
             client.mutate({
               mutation: updateOrgUserMutation,
               variables: {
-                nodeId: u.nodeId,
-                organizationId: u.organizationId,
+                nodeId: user.nodeId,
+                organizationId: user.organizationId,
                 userId,
-                role: u.role,
+                role: user.role,
               },
             })
             orgUsersData.refetch()
@@ -82,15 +67,15 @@ const OrgUsers = ({
         />
         <AutocompleteFromArray
           label="Rolle"
-          valueText={u.role}
+          valueText={user.role}
           dataSource={roles}
           updatePropertyInDb={role => {
             client.mutate({
               mutation: updateOrgUserMutation,
               variables: {
-                nodeId: u.nodeId,
-                organizationId: u.organizationId,
-                userId: u.userId,
+                nodeId: user.nodeId,
+                organizationId: user.organizationId,
+                userId: user.userId,
                 role,
               },
             })
@@ -102,7 +87,7 @@ const OrgUsers = ({
             await client.mutate({
               mutation: deleteOrgUserMutation,
               variables: {
-                id: u.id,
+                id: user.id,
               },
             })
           }}
--- src/components/Organisation/OrgUsers/index.js
+++ src/components/Organisation/OrgUsers/index.js
@@ -6,17 +6,14 @@ import get from 'lodash/get'
 import sortBy from 'lodash/sortBy'
 import { withApollo } from 'react-apollo'
 import IconButton from 'material-ui/IconButton'
-import ContentClear from 'material-ui/svg-icons/content/clear'
 import ContentAdd from 'material-ui/svg-icons/content/add'
 import { red500 } from 'material-ui/styles/colors'
 
 import activeNodeArrayData from '../../../modules/activeNodeArrayData'
 import orgUsersData from './orgUsersData'
-import AutocompleteFromArray from '../../shared/AutocompleteFromArray'
-import updateOrgUserMutation from './updateOrgUserMutation'
-import deleteOrgUserMutation from './deleteOrgUserMutation'
 import createOrgUserMutation from './createOrgUserMutation'
 import ErrorBoundary from '../../shared/ErrorBoundary'
+import OrgUser from './OrgUser'
 
 const Container = styled.div`
   display: flex;
@@ -27,12 +24,6 @@ const Container = styled.div`
 const List = styled.div`
   display: flex;
   flex-direction: column;
-`
-const OrgUserDiv = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: flex-end;
 `
 const DeleteButton = styled(IconButton)`
   top: 10px !important;
@@ -52,11 +43,6 @@ const OrgUsers = ({
     'organizationByName.organizationUsersByOrganizationId.nodes',
     []
   )
-  const users = get(orgUsersData, 'allUsers.nodes', [])
-  const userNames = users.map(u => u.name).sort()
-  const roles = get(orgUsersData, 'allRoles.nodes', [])
-    .map(u => u.name)
-    .sort()
   /**
    * TODO: use state
    * initiate at componentDidMount
@@ -69,60 +55,15 @@ const OrgUsers = ({
         <List>
           {sortBy(
             orgUsers,
-            u =>
-              `${u.userByUserId ? u.userByUserId.name : 'zzzzz'}${
-                u.role ? u.role : 'zzzzz'
+            user =>
+              `${user.userByUserId ? user.userByUserId.name : 'zzzzz'}${
+                user.role ? user.role : 'zzzzz'
               }`
-          ).map(u => (
-            <OrgUserDiv key={`${get(u, 'userByUserId.id')}/${u.role}`}>
-              <AutocompleteFromArray
-                label="Benutzer"
-                valueText={get(u, 'userByUserId.name')}
-                dataSource={userNames}
-                updatePropertyInDb={val => {
-                  const userId = users.find(u => u.name === val).id
-                  client.mutate({
-                    mutation: updateOrgUserMutation,
-                    variables: {
-                      nodeId: u.nodeId,
-                      organizationId: u.organizationId,
-                      userId,
-                      role: u.role,
-                    },
-                  })
-                  orgUsersData.refetch()
-                }}
-              />
-              <AutocompleteFromArray
-                label="Rolle"
-                valueText={u.role}
-                dataSource={roles}
-                updatePropertyInDb={role => {
-                  client.mutate({
-                    mutation: updateOrgUserMutation,
-                    variables: {
-                      nodeId: u.nodeId,
-                      organizationId: u.organizationId,
-                      userId: u.userId,
-                      role,
-                    },
-                  })
-                }}
-              />
-              <IconButton
-                tooltip="löschen"
-                onClick={async () => {
-                  await client.mutate({
-                    mutation: deleteOrgUserMutation,
-                    variables: {
-                      id: u.id,
-                    },
-                  })
-                }}
-              >
-                <ContentClear color={red500} />
-              </IconButton>
-            </OrgUserDiv>
+          ).map(user => (
+            <OrgUser
+              user={user}
+              key={`${get(user, 'userByUserId.id')}/${user.role}`}
+            />
           ))}
           <DeleteButton
             tooltip="Neue Rolle vergeben"
