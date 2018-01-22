d54530de247bff515b3c95e0aa4e0d0c0a99be38
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-22 20:17:54 +0100

    simplify

--- src/components/Organisation/OrgUsers/orgUsersData.js
+++ src/components/Organisation/OrgUsers/orgUsersData.js
@@ -1,14 +1,49 @@
 // @flow
 import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 import get from 'lodash/get'
 
 import orgUsersGql from './orgUsersGql'
 
-export default graphql(orgUsersGql, {
-  options: ({ activeNodeArrayData }) => ({
-    variables: {
-      name: get(activeNodeArrayData, 'activeNodeArray', ['none', 'none'])[1],
-    },
-  }),
-  name: 'orgUsersData',
-})
+export default graphql(
+  gql`
+    query orgUsersQuery($name: String!) {
+      organizationByName(name: $name) {
+        id
+        organizationUsersByOrganizationId {
+          totalCount
+          nodes {
+            id
+            organizationId
+            userId
+            nodeId
+            userByUserId {
+              id
+              name
+            }
+            role
+          }
+        }
+      }
+      allUsers {
+        nodes {
+          id
+          name
+        }
+      }
+      allRoles {
+        nodes {
+          name
+        }
+      }
+    }
+  `,
+  {
+    options: ({ activeNodeArrayData }) => ({
+      variables: {
+        name: get(activeNodeArrayData, 'activeNodeArray', ['none', 'none'])[1],
+      },
+    }),
+    name: 'orgUsersData',
+  }
+)
