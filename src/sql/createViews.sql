2bb29d33ace103d36102677a81f3bb1d83217abf
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-24 14:34:33 +0100

    work on policies

--- src/sql/createViews.sql
+++ src/sql/createViews.sql
@@ -38,15 +38,6 @@ from
 where
   ae.organization_user.role in ('orgAdmin');
 
-create or replace view ae.taxonomy_writers as
-select distinct ae.user.name
-from
-  ae.organization_user
-  inner join ae.user
-  on ae.user.id = ae.organization_user.user_id
-where
-  ae.organization_user.role in ('orgTaxonomyWriter', 'orgAdmin');
-
 create or replace view ae.collection_writers as
 select distinct ae.user.name
 from
@@ -65,6 +56,16 @@ FROM
 where
   ae.user.name = current_user_name()
   and ae.organization_user.role in ('orgCollectionWriter', 'orgAdmin');
+
+create or replace view ae.organizations_currentuser_is_orgadmin as
+select distinct ae.organization_user.organization_id
+FROM
+  ae.organization_user
+  inner join ae.user
+  on ae.user.id = ae.organization_user.user_id
+where
+  ae.user.name = current_user_name()
+  and ae.organization_user.role in ('orgAdmin');
 
 create or replace view ae.current_user_writable_collections as
 select distinct ae.property_collection.id
