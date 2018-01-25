841d5c7845e01b7fd3c0ca33967794814b274aa8
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-25 11:37:03 +0100

    begin creating objects

---
+++ src/components/Objekt/createObjectMutation.js
@@ -0,0 +1,14 @@
+// @flow
+import gql from 'graphql-tag'
+
+export default gql`
+  mutation createObject($taxonomyId: UUID!, parentId: UUID!) {
+    createObject(input: { object: { taxonomyId: $taxonomyId, parentId: $parentId} }) {
+      object {
+        id
+        taxonomyId
+        parentId
+      }
+    }
+  }
+`
