5c00add72f97000fc8cc5582ffa7db41101312d3
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-29 15:32:01 +0100

    work on refactoring categories

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -57,7 +57,7 @@ CREATE TABLE ae.taxonomy (
   habitat_nr_fns_max integer DEFAULT NULL,
   CONSTRAINT proper_links CHECK (length(regexp_replace(array_to_string(links, ''),'((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)',''))=0)
 );
-
+-- once: alter table ae.taxonomy add typ taxonomy_type DEFAULT NULL;
 CREATE INDEX ON ae.taxonomy USING btree (name);
 CREATE INDEX ON ae.taxonomy USING btree (category);
 
