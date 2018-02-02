935cdebf38f49883db7f6d90abb23098ead8dfc1
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-02 10:35:48 +0100

    work on tax mutation

---
+++ src/components/Taxonomy/Property.js
@@ -0,0 +1,89 @@
+// @flow
+import React from 'react'
+import TextField from 'material-ui-next/TextField'
+import styled from 'styled-components'
+import compose from 'recompose/compose'
+import withHandlers from 'recompose/withHandlers'
+import withState from 'recompose/withState'
+import { withApollo } from 'react-apollo'
+
+import ErrorBoundary from '../../shared/ErrorBoundary'
+import updateObjectMutation from '../updateObjectMutation'
+
+const Container = styled.div`
+  margin: 12px 10px;
+`
+
+const enhance = compose(
+  withApollo,
+  withState('value', 'setValue', ({ objekt, field }) => objekt[field] || ''),
+  withHandlers({
+    onChange: ({ setValue }) => event => setValue(event.target.value),
+    onBlur: ({ client, field, objekt }) => event => {
+      const { value } = event.target
+      if (value !== 'prevValue') {
+        client.mutate({
+          mutation: updateObjectMutation,
+          variables: {
+            name: value,
+            id: objekt.id,
+          },
+          optimisticResponse: {
+            updateObjectById: {
+              object: {
+                id: objekt.id,
+                name: value,
+                __typename: 'Object',
+              },
+              __typename: 'Object',
+            },
+            __typename: 'Mutation',
+          },
+        })
+      }
+    },
+  })
+)
+
+const Property = ({
+  client,
+  field,
+  label,
+  objekt,
+  value,
+  disabled,
+  onChange,
+  onBlur,
+}: {
+  client: Object,
+  field: String,
+  label: String,
+  objekt: Object,
+  disabled: Boolean,
+  value: String,
+  onChange: () => void,
+  onBlur: () => void,
+}) => {
+  return (
+    <ErrorBoundary>
+      <Container>
+        <TextField
+          autoFocus={label === 'Name' && !value}
+          label={label}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          fullWidth
+          multiline
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+          spellCheck="false"
+          disabled={!!disabled}
+        />
+      </Container>
+    </ErrorBoundary>
+  )
+}
+
+export default enhance(Property)
---
+++ src/components/Taxonomy/updateTaxonomyMutationLr.js
@@ -0,0 +1,24 @@
+// @flow
+import gql from 'graphql-tag'
+
+export default gql`
+  mutation updateTaxonomy($id: UUID!, $name: String, $description: String, links: [String], organizationId: UUID, lastUpdated: Date, importedBy: UUID, termsOfUse: String, type: TaxonomyType, habitatLabel: String, habitatComments: String, habitatNrFnsMin: Int, habitatNrFnsMax: Int) {
+    updateTaxonomyById(input: { id: $id, taxonomyPatch: { name: $name, description: $description, links: $links, organizationId: $organizationId, lastUpdated: $lastUpdated, importedBy: $importedBy, termsOfUse: $termsOfUse, type: $type, habitatLabel: $habitatLabel, habitatComments: $habitatComments, habitatNrFnsMin: $habitatNrFnsMin, habitatNrFnsMax: $habitatNrFnsMax } }) {
+      taxonomy {
+        id
+        name
+        description
+        links
+        organizationId
+        lastUpdated
+        importedBy
+        termsOfUse
+        type
+        habitatLabel
+        habitatComments
+        habitatNrFnsMin
+        habitatNrFnsMax
+      }
+    }
+  }
+`
