b71fc99f7bd2b6d6345513bc376beb36673d35be
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-02 10:13:13 +0100

    update policy views

--- src/sql/createViews.sql
+++ src/sql/createViews.sql
@@ -1,29 +1,35 @@
-create or replace view ae.current_user_writable_taxonomies as
-select distinct ae.taxonomy.id
-from  
-  ae.taxonomy
-  inner join ae.organization_user
-    inner join ae.user
-    on ae.user.id = ae.organization_user.user_id
-  on ae.organization_user.organization_id = ae.taxonomy.organization_id
-where
-  ae.user.name = current_user_name()
-  and ae.organization_user.role in ('orgTaxonomyWriter', 'orgAdmin');
 
+
+create or replace view ae.taxonomy_writers as
+select distinct ae.user.name
+from
+  ae.organization_user
+  inner join ae.user
+  on ae.user.id = ae.organization_user.user_id
+where
+  ae.organization_user.role in ('orgTaxonomyWriter', 'orgAdmin');
+  
+create or replace view ae.current_user_writable_taxonomies as
 select distinct ae.taxonomy.id
 from ae.taxonomy
 where ae.taxonomy.id in (
 	select distinct ae.taxonomy.id
-	from  
-    ae.taxonomy
+	from ae.taxonomy
     inner join ae.organization_user
-              inner join ae.user
-              on ae.user.id = ae.organization_user.user_id
+      inner join ae.user
+      on ae.user.id = ae.organization_user.user_id
     on ae.organization_user.organization_id = ae.taxonomy.organization_id
 	where
-						ae.user.name = 'Alexander Gabriel'
-						and ae.organization_user.role in ('orgTaxonomyWriter', 'orgAdmin')
-	) or ae.taxonomy.id in (select taxonomy.id from ae.taxonomy where organization_id is null)
+    ae.user.name = current_user_name()
+    and ae.organization_user.role in ('orgTaxonomyWriter', 'orgAdmin')
+	)
+  or
+  (
+    ae.taxonomy.id in (
+      select taxonomy.id from ae.taxonomy where organization_id is null
+    )
+    AND current_user_name() IN (SELECT * FROM ae.taxonomy_writers)
+  );
 
 create or replace view ae.organizations_currentuser_is_taxonomywriter as
 select distinct ae.organization_user.organization_id
@@ -34,15 +40,6 @@ FROM
 where
   ae.user.name = current_user_name()
   and ae.organization_user.role in ('orgTaxonomyWriter', 'orgAdmin');
-
-create or replace view ae.taxonomy_writers as
-select distinct ae.user.name
-from
-  ae.organization_user
-  inner join ae.user
-  on ae.user.id = ae.organization_user.user_id
-where
-  ae.organization_user.role in ('orgTaxonomyWriter', 'orgAdmin');
 
 create or replace view ae.organization_admins as
 select distinct ae.user.name
