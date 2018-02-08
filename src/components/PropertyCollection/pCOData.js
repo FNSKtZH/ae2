094d6c7421570c8bc8e334d191b49c5053bbebeb
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-08 10:49:21 +0100

    simplify

--- src/components/PropertyCollection/pCOData.js
+++ src/components/PropertyCollection/pCOData.js
@@ -1,18 +1,52 @@
 // @flow
 import { graphql } from 'react-apollo'
 import get from 'lodash/get'
+import gql from 'graphql-tag'
 
-import pCOGql from './pCOGql'
-
-export default graphql(pCOGql, {
-  options: ({ activeNodeArrayData }) => ({
-    variables: {
-      pCId: get(
-        activeNodeArrayData,
-        'activeNodeArray[1]',
-        '99999999-9999-9999-9999-999999999999'
-      ),
-    },
-  }),
-  name: 'pCOData',
-})
+export default graphql(
+  gql`
+    query pCOQuery($pCId: UUID!) {
+      propertyCollectionById(id: $pCId) {
+        id
+        organizationByOrganizationId {
+          id
+          name
+          organizationUsersByOrganizationId {
+            nodes {
+              userId
+              role
+              userByUserId {
+                id
+                name
+                email
+              }
+            }
+          }
+        }
+        propertyCollectionObjectsByPropertyCollectionId {
+          totalCount
+          nodes {
+            id
+            objectId
+            objectByObjectId {
+              name
+            }
+            properties
+          }
+        }
+      }
+    }
+  `,
+  {
+    options: ({ activeNodeArrayData }) => ({
+      variables: {
+        pCId: get(
+          activeNodeArrayData,
+          'activeNodeArray[1]',
+          '99999999-9999-9999-9999-999999999999'
+        ),
+      },
+    }),
+    name: 'pCOData',
+  }
+)
