ee83dd8ea89b3828a700a55757957ef7af207ab2
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-11 12:23:54 +0100

    modularize

--- src/components/Export/ChooseColumn/Filter/PcosCard/index.js
+++ src/components/Export/ChooseColumn/Filter/PcosCard/index.js
@@ -7,8 +7,7 @@ import get from 'lodash/get'
 import groupBy from 'lodash/groupBy'
 import compose from 'recompose/compose'
 
-import PcoField from '../PcoField'
-import constants from '../../../../../modules/constants'
+import PcoCard from './PcoCard'
 import propsByTaxData from '../../propsByTaxData'
 import exportTaxonomiesData from '../../../exportTaxonomiesData'
 import ErrorBoundary from '../../../../shared/ErrorBoundary'
@@ -20,38 +19,15 @@ const Level2Card = styled(Card)`
     padding-bottom: 0 !important;
   }
 `
-const Level3Card = styled(Card)`
-  margin: 0;
-  padding: 0;
-`
 const Level2CardHeader = styled(CardHeader)`
   background-color: #ffcc80;
 `
-const Level3CardHeader = styled(CardHeader)`
-  background-color: #fff3e0;
-  border-bottom: 1px solid #ebebeb;
-`
 const Level2CardText = styled(CardText)`
-  padding: 0 !important;
-`
-const Level3CardText = styled(CardText)`
   padding: 0 !important;
 `
 const Level2Count = styled.span`
   font-size: x-small;
   padding-left: 5px;
-`
-const Level3Count = styled.span`
-  font-size: x-small;
-  padding-left: 5px;
-`
-const PropertiesContainer = styled.div`
-  margin: 8px 0;
-  padding-bottom: 10px;
-  column-width: ${props =>
-    props['data-width'] > 2 * constants.export.properties.columnWidth
-      ? `${constants.export.properties.columnWidth}px`
-      : 'auto'};
 `
 
 const level2CardTitleStyle = { fontWeight: 'bold' }
@@ -102,37 +78,7 @@ const PcosCard = ({
         />
         <Level2CardText expandable={true}>
           {Object.keys(pcoPropertiesByPropertyCollection).map(pc => (
-            <Level3Card key={pc}>
-              <Level3CardHeader
-                title={
-                  <div>
-                    {pc}
-                    <Level3Count>{`(${
-                      pcoPropertiesByPropertyCollection[pc].length
-                    } ${
-                      pcoPropertiesByPropertyCollection[pc].length === 1
-                        ? 'Feld'
-                        : 'Felder'
-                    })`}</Level3Count>
-                  </div>
-                }
-                actAsExpander={true}
-                showExpandableButton={true}
-                titleStyle={level2CardTitleStyle}
-              />
-              <Level3CardText expandable={true}>
-                <PropertiesContainer data-width={window.innerWidth - 84}>
-                  {pcoPropertiesByPropertyCollection[pc].map(field => (
-                    <PcoField
-                      key={`${field.propertyName}${field.jsontype}`}
-                      pcname={field.propertyCollectionName}
-                      pname={field.propertyName}
-                      jsontype={field.jsontype}
-                    />
-                  ))}
-                </PropertiesContainer>
-              </Level3CardText>
-            </Level3Card>
+            <PcoCard key={pc} pc={pc} />
           ))}
         </Level2CardText>
       </Level2Card>
