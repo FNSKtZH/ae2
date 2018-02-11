e4da352b17495f9962552dbf1544f89671005032
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-11 12:32:50 +0100

    rename

--- src/components/Export/ChooseColumn/Filter/PcosCard/index.js
+++ src/components/Export/ChooseColumn/Filter/PcosCard/index.js
@@ -12,25 +12,25 @@ import propsByTaxData from '../../propsByTaxData'
 import exportTaxonomiesData from '../../../exportTaxonomiesData'
 import ErrorBoundary from '../../../../shared/ErrorBoundary'
 
-const Level2Card = styled(Card)`
+const StyledCard = styled(Card)`
   margin: 10px 0;
   padding: 0;
   > div {
     padding-bottom: 0 !important;
   }
 `
-const Level2CardHeader = styled(CardHeader)`
+const StyledCardHeader = styled(CardHeader)`
   background-color: #ffcc80;
 `
-const Level2CardText = styled(CardText)`
+const StyledCardText = styled(CardText)`
   padding: 0 !important;
 `
-const Level2Count = styled.span`
+const Count = styled.span`
   font-size: x-small;
   padding-left: 5px;
 `
 
-const level2CardTitleStyle = { fontWeight: 'bold' }
+const cardTitleStyle = { fontWeight: 'bold' }
 
 const enhance = compose(withApollo, exportTaxonomiesData, propsByTaxData)
 
@@ -57,31 +57,31 @@ const PcosCard = ({
 
   return (
     <ErrorBoundary>
-      <Level2Card expanded={pcoExpanded} onExpandChange={onTogglePco}>
-        <Level2CardHeader
+      <StyledCard expanded={pcoExpanded} onExpandChange={onTogglePco}>
+        <StyledCardHeader
           title={
             <div>
               Eigenschaftensammlungen{pCCount > 0 && (
-                <Level2Count>{`(${pCCount} Sammlungen, ${
+                <Count>{`(${pCCount} Sammlungen, ${
                   Object.keys(pcoPropertiesFields).length
                 } ${
                   Object.keys(pcoPropertiesFields).length === 1
                     ? 'Feld'
                     : 'Felder'
-                })`}</Level2Count>
+                })`}</Count>
               )}
             </div>
           }
           actAsExpander={true}
           showExpandableButton={true}
-          titleStyle={level2CardTitleStyle}
+          titleStyle={cardTitleStyle}
         />
-        <Level2CardText expandable={true}>
+        <StyledCardText expandable={true}>
           {Object.keys(pcoPropertiesByPropertyCollection).map(pc => (
             <PcoCard key={pc} pc={pc} />
           ))}
-        </Level2CardText>
-      </Level2Card>
+        </StyledCardText>
+      </StyledCard>
     </ErrorBoundary>
   )
 }
