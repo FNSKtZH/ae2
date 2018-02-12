e8d6ce6c994b3f1cb873447e8135f4c4bd79c07f
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-12 17:54:48 +0100

    upgrade material

--- src/components/Export/ChooseColumn/Properties/HowTo.js
+++ src/components/Export/ChooseColumn/Properties/HowTo.js
@@ -1,35 +1,74 @@
 // @flow
 import React from 'react'
-import { Card, CardHeader, CardText } from 'material-ui/Card'
+import Card, { CardActions, CardContent } from 'material-ui-next/Card'
+import Collapse from 'material-ui-next/transitions/Collapse'
+import IconButton from 'material-ui-next/IconButton'
+import Icon from 'material-ui-next/Icon'
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore'
+import compose from 'recompose/compose'
+import withState from 'recompose/withState'
+import styled from 'styled-components'
 
-const level1CardStyle = { margin: '10px 0' }
-const level1CardTitleStyle = { fontWeight: 'bold' }
-const level1CardHeaderStyle = {}
-const level1CardTextStyle = {
-  padding: '0 16px !important',
-  margin: '-10px 10px -5px 0',
-}
+const StyledCard = styled(Card)`
+  margin: 10px 0;
+  background-color: rgb(255, 243, 224) !important;
+`
+const StyledCardActions = styled(CardActions)`
+  justify-content: space-between;
+  cursor: pointer;
+`
+const CardActionIconButton = styled(IconButton)`
+  transform: ${props => (props['data-expanded'] ? 'rotate(180deg)' : 'none')};
+`
+const CardActionTitle = styled.div`
+  padding-left: 8px;
+  font-weight: bold;
+`
+const StyledCardContent = styled(CardContent)`
+  padding: 0 16px 0 0 !important;
+  margin: -10px 0 5px 0;
+`
 
-const HowToProperties = () => (
-  <Card style={level1CardStyle}>
-    <CardHeader
-      title="So geht's"
-      actAsExpander={true}
-      showExpandableButton={true}
-      titleStyle={level1CardTitleStyle}
-      style={level1CardHeaderStyle}
-    />
-    <CardText expandable={true} style={level1CardTextStyle}>
-      <ul>
-        <li>
-          Nachfolgend sind alle Eigenschaften aufgelistet, die in den gewählten
-          Taxonomien vorkommen
-        </li>
-        <li>Markieren Sie die Eigenschaften, die Sie exportieren möchten...</li>
-        <li>...und laden Sie danach die Daten herunter</li>
-      </ul>
-    </CardText>
-  </Card>
+const enhance = compose(withState('expanded', 'setExpanded', false))
+
+const HowToProperties = ({
+  expanded,
+  setExpanded,
+}: {
+  expanded: Boolean,
+  setExpanded: () => void,
+}) => (
+  <StyledCard>
+    <StyledCardActions
+      disableActionSpacing
+      onClick={() => setExpanded(!expanded)}
+    >
+      <CardActionTitle>So geht's</CardActionTitle>
+      <CardActionIconButton
+        data-expanded={expanded}
+        aria-expanded={expanded}
+        aria-label="Show more"
+      >
+        <Icon>
+          <ExpandMoreIcon />
+        </Icon>
+      </CardActionIconButton>
+    </StyledCardActions>
+    <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <StyledCardContent>
+        <ul>
+          <li>
+            Nachfolgend sind alle Eigenschaften aufgelistet, die in den
+            gewählten Taxonomien vorkommen
+          </li>
+          <li>
+            Markieren Sie die Eigenschaften, die Sie exportieren möchten...
+          </li>
+          <li>...und laden Sie danach die Daten herunter</li>
+        </ul>
+      </StyledCardContent>
+    </Collapse>
+  </StyledCard>
 )
 
-export default HowToProperties
+export default enhance(HowToProperties)
