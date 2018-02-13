a22c425de35567fdaa0f5348b5f8ca048a6227ba
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-13 10:53:18 +0100

    modularize

---
+++ src/components/Export/ChooseColumn/Properties/RCOs/index.js
@@ -0,0 +1,201 @@
+// @flow
+import React from 'react'
+import { Card, CardHeader, CardText } from 'material-ui/Card'
+import styled from 'styled-components'
+import { withApollo } from 'react-apollo'
+import get from 'lodash/get'
+import groupBy from 'lodash/groupBy'
+import compose from 'recompose/compose'
+
+import AllRcoChooser from '../AllRcoChooser'
+import RcoChooser from '../RcoChooser'
+import constants from '../../../../../modules/constants'
+import propsByTaxData from '../../propsByTaxData'
+import exportTaxonomiesData from '../../../exportTaxonomiesData'
+import data from '../data'
+import ErrorBoundary from '../../../../shared/ErrorBoundary'
+
+const Level2Card = styled(Card)`
+  margin: 10px 0;
+  padding: 0;
+  > div {
+    padding-bottom: 0 !important;
+  }
+`
+const Level3Card = styled(Card)`
+  margin: 0;
+  padding: 0;
+`
+const Level2CardHeader = styled(CardHeader)`
+  background-color: #ffcc80;
+`
+const Level3CardHeader = styled(CardHeader)`
+  background-color: #fff3e0;
+  border-bottom: 1px solid #ebebeb;
+`
+const Level2CardText = styled(CardText)`
+  padding: 0 !important;
+`
+const Level3CardText = styled(CardText)`
+  display: flex;
+  flex-direction: column;
+`
+const PropertiesContainer = styled.div`
+  column-width: ${props =>
+    props['data-width'] > 2 * constants.export.properties.columnWidth
+      ? `${constants.export.properties.columnWidth}px`
+      : 'auto'};
+`
+const Level2Count = styled.span`
+  font-size: x-small;
+  padding-left: 5px;
+`
+const Level3Count = styled.span`
+  font-size: x-small;
+  padding-left: 5px;
+`
+
+const level2CardTitleStyle = { fontWeight: 'bold' }
+
+const enhance = compose(withApollo, exportTaxonomiesData, data, propsByTaxData)
+
+const RCOs = ({
+  propsByTaxData,
+  data,
+  rcoExpanded,
+  onToggleRco,
+}: {
+  propsByTaxData: Object,
+  data: Object,
+  rcoExpanded: Boolean,
+  onToggleRco: () => {},
+}) => {
+  const rcoProperties = get(
+    propsByTaxData,
+    'rcoPropertiesByTaxonomiesFunction.nodes',
+    []
+  )
+
+  const rcoPropertiesByPropertyCollection = groupBy(rcoProperties, x => {
+    if (x.propertyCollectionName.includes(x.relationType)) {
+      return x.propertyCollectionName
+    }
+    return `${x.propertyCollectionName}: ${x.relationType}`
+  })
+  // TODO: need to add BeziehungsPartnerId and BeziehungsPartnerName
+  const rcoCountByTaxonomyRelationType = get(
+    data,
+    'rcoCountByTaxonomyRelationTypeFunction.nodes',
+    []
+  )
+  // TODO:
+  // in every key of rcoPropertiesByPropertyCollection
+  // add id and name of Beziehungspartner
+
+  Object.values(rcoPropertiesByPropertyCollection).forEach(rpc => {
+    const myRpc = rpc[0] || {}
+    let rco = rcoCountByTaxonomyRelationType.find(
+      r =>
+        r.propertyCollectionName === myRpc.propertyCollectionName &&
+        r.relationType === myRpc.relationType
+    )
+    if (!rco) {
+      rco = rcoCountByTaxonomyRelationType.find(
+        r =>
+          `${r.propertyCollectionName}: ${r.relationType}` ===
+            myRpc.propertyCollectionName &&
+          r.relationType === myRpc.relationType
+      )
+    }
+    if (!rco) rco = {}
+    rpc.push({
+      count: rco.count,
+      jsontype: 'String',
+      propertyCollectionName: myRpc.propertyCollectionName,
+      propertyName: 'Beziehungspartner_id',
+      relationType: myRpc.relationType,
+    })
+    rpc.push({
+      count: rco.count,
+      jsontype: 'String',
+      propertyCollectionName: myRpc.propertyCollectionName,
+      propertyName: 'Beziehungspartner_Name',
+      relationType: myRpc.relationType,
+    })
+  })
+  const rcoPropertiesFields = groupBy(rcoProperties, 'propertyName')
+  //console.log('RCOs: pcoPropertiesFields:', pcoPropertiesFields)
+  /*console.log(
+    'RCOs: rcoPropertiesByPropertyCollection:',
+    rcoPropertiesByPropertyCollection
+  )*/
+  const rCCount = Object.keys(rcoPropertiesByPropertyCollection).length
+
+  return (
+    <ErrorBoundary>
+      <Level2Card expanded={rcoExpanded} onExpandChange={onToggleRco}>
+        <Level2CardHeader
+          title={
+            <div>
+              Beziehungssammlungen{rCCount > 0 && (
+                <Level2Count>{`(${rCCount} Sammlungen, ${
+                  Object.keys(rcoPropertiesFields).length
+                } ${
+                  Object.keys(rcoPropertiesFields).length === 1
+                    ? 'Feld'
+                    : 'Felder'
+                })`}</Level2Count>
+              )}
+            </div>
+          }
+          actAsExpander={true}
+          showExpandableButton={true}
+          titleStyle={level2CardTitleStyle}
+        />
+        <Level2CardText expandable={true}>
+          {Object.keys(rcoPropertiesByPropertyCollection).map(pc => (
+            <Level3Card key={pc}>
+              <Level3CardHeader
+                title={
+                  <div>
+                    {pc}
+                    <Level3Count>{`(${
+                      rcoPropertiesByPropertyCollection[pc].length
+                    } ${
+                      rcoPropertiesByPropertyCollection[pc].length === 1
+                        ? 'Feld'
+                        : 'Felder'
+                    })`}</Level3Count>
+                  </div>
+                }
+                actAsExpander={true}
+                showExpandableButton={true}
+                titleStyle={level2CardTitleStyle}
+              />
+              <Level3CardText expandable={true}>
+                {rcoPropertiesByPropertyCollection[pc].length > 1 && (
+                  <AllRcoChooser
+                    properties={rcoPropertiesByPropertyCollection[pc]}
+                  />
+                )}
+                <PropertiesContainer data-width={window.innerWidth - 84}>
+                  {rcoPropertiesByPropertyCollection[pc].map(field => (
+                    <RcoChooser
+                      key={`${field.propertyName}${field.jsontype}`}
+                      pcname={field.propertyCollectionName}
+                      pname={field.propertyName}
+                      jsontype={field.jsontype}
+                      count={field.count}
+                    />
+                  ))}
+                </PropertiesContainer>
+              </Level3CardText>
+            </Level3Card>
+          ))}
+        </Level2CardText>
+      </Level2Card>
+    </ErrorBoundary>
+  )
+}
+
+export default enhance(RCOs)
--- src/components/Export/ChooseColumn/Properties/index.js
+++ src/components/Export/ChooseColumn/Properties/index.js
@@ -1,10 +1,7 @@
 // @flow
 import React from 'react'
-import { Card, CardHeader, CardText } from 'material-ui/Card'
 import styled from 'styled-components'
 import { withApollo } from 'react-apollo'
-import get from 'lodash/get'
-import groupBy from 'lodash/groupBy'
 import compose from 'recompose/compose'
 import withState from 'recompose/withState'
 import withHandlers from 'recompose/withHandlers'
@@ -12,9 +9,7 @@ import withHandlers from 'recompose/withHandlers'
 import HowTo from './HowTo'
 import Taxonomies from './Taxonomies'
 import PCOs from './PCOs'
-import AllRcoChooser from './AllRcoChooser'
-import RcoChooser from './RcoChooser'
-import constants from '../../../../modules/constants'
+import RCOs from './RCOs'
 import propsByTaxData from '../propsByTaxData'
 import exportTaxonomiesData from '../../exportTaxonomiesData'
 import data from './data'
@@ -24,74 +19,18 @@ const Container = styled.div`
   padding: 5px 10px;
   overflow: auto !important;
 `
-const Level2Card = styled(Card)`
-  margin: 10px 0;
-  padding: 0;
-  > div {
-    padding-bottom: 0 !important;
-  }
-`
-const Level3Card = styled(Card)`
-  margin: 0;
-  padding: 0;
-`
-const Level2CardHeader = styled(CardHeader)`
-  background-color: #ffcc80;
-`
-const Level3CardHeader = styled(CardHeader)`
-  background-color: #fff3e0;
-  border-bottom: 1px solid #ebebeb;
-`
-const Level2CardText = styled(CardText)`
-  padding: 0 !important;
-`
-const Level3CardText = styled(CardText)`
-  display: flex;
-  flex-direction: column;
-`
-const PropertiesContainer = styled.div`
-  column-width: ${props =>
-    props['data-width'] > 2 * constants.export.properties.columnWidth
-      ? `${constants.export.properties.columnWidth}px`
-      : 'auto'};
-`
-const Level2Count = styled.span`
-  font-size: x-small;
-  padding-left: 5px;
-`
-const Level3Count = styled.span`
-  font-size: x-small;
-  padding-left: 5px;
-`
-
-const level2CardTitleStyle = { fontWeight: 'bold' }
 
 const enhance = compose(
   withApollo,
   exportTaxonomiesData,
   data,
   propsByTaxData,
-  withState('jointTaxonomiesExpanded', 'setJointTaxonomiesExpanded', false),
   withState('taxonomiesExpanded', 'setTaxonomiesExpanded', false),
   withState('pcoExpanded', 'setFilterExpanded', false),
   withState('rcoExpanded', 'setPropertiesExpanded', false),
   withHandlers({
-    onToggleJointTaxonomies: ({
-      jointTaxonomiesExpanded,
-      setJointTaxonomiesExpanded,
-      setTaxonomiesExpanded,
-      setFilterExpanded,
-      setPropertiesExpanded,
-    }) => () => {
-      setJointTaxonomiesExpanded(!jointTaxonomiesExpanded)
-      // close all others
-      setTaxonomiesExpanded(false)
-      setFilterExpanded(false)
-      setPropertiesExpanded(false)
-    },
     onToggleTaxonomies: ({
       taxonomiesExpanded,
-      setJointTaxonomiesExpanded,
       setTaxonomiesExpanded,
       setFilterExpanded,
       setPropertiesExpanded,
@@ -102,13 +41,11 @@ const enhance = compose(
       // if so: open it
 
       // close all others
-      setJointTaxonomiesExpanded(false)
       setFilterExpanded(false)
       setPropertiesExpanded(false)
     },
     onTogglePco: ({
       pcoExpanded,
-      setJointTaxonomiesExpanded,
       setTaxonomiesExpanded,
       setFilterExpanded,
       setPropertiesExpanded,
@@ -116,7 +53,6 @@ const enhance = compose(
       if (!pcoExpanded) {
         setFilterExpanded(true)
         // close all others
-        setJointTaxonomiesExpanded(false)
         setTaxonomiesExpanded(false)
         setPropertiesExpanded(false)
       } else {
@@ -125,7 +61,6 @@ const enhance = compose(
     },
     onToggleRco: ({
       rcoExpanded,
-      setJointTaxonomiesExpanded,
       setTaxonomiesExpanded,
       setFilterExpanded,
       setPropertiesExpanded,
@@ -133,7 +68,6 @@ const enhance = compose(
       if (!rcoExpanded) {
         setPropertiesExpanded(true)
         // close all others
-        setJointTaxonomiesExpanded(false)
         setTaxonomiesExpanded(false)
         setFilterExpanded(false)
       } else {
@@ -147,160 +81,33 @@ const enhance = compose(
 const Properties = ({
   propsByTaxData,
   data,
-  jointTaxonomiesExpanded,
   taxonomiesExpanded,
   pcoExpanded,
   rcoExpanded,
-  onToggleJointTaxonomies,
   onToggleTaxonomies,
   onTogglePco,
   onToggleRco,
 }: {
   propsByTaxData: Object,
   data: Object,
-  onToggleJointTaxonomies: Boolean,
-  jointTaxonomiesExpanded: Boolean,
   taxonomiesExpanded: Boolean,
   pcoExpanded: Boolean,
   rcoExpanded: Boolean,
   onToggleTaxonomies: () => {},
   onTogglePco: () => {},
   onToggleRco: () => {},
-}) => {
-  const rcoProperties = get(
-    propsByTaxData,
-    'rcoPropertiesByTaxonomiesFunction.nodes',
-    []
-  )
-
-  const rcoPropertiesByPropertyCollection = groupBy(rcoProperties, x => {
-    if (x.propertyCollectionName.includes(x.relationType)) {
-      return x.propertyCollectionName
-    }
-    return `${x.propertyCollectionName}: ${x.relationType}`
-  })
-  // TODO: need to add BeziehungsPartnerId and BeziehungsPartnerName
-  const rcoCountByTaxonomyRelationType = get(
-    data,
-    'rcoCountByTaxonomyRelationTypeFunction.nodes',
-    []
-  )
-  // TODO:
-  // in every key of rcoPropertiesByPropertyCollection
-  // add id and name of Beziehungspartner
-
-  Object.values(rcoPropertiesByPropertyCollection).forEach(rpc => {
-    const myRpc = rpc[0] || {}
-    let rco = rcoCountByTaxonomyRelationType.find(
-      r =>
-        r.propertyCollectionName === myRpc.propertyCollectionName &&
-        r.relationType === myRpc.relationType
-    )
-    if (!rco) {
-      rco = rcoCountByTaxonomyRelationType.find(
-        r =>
-          `${r.propertyCollectionName}: ${r.relationType}` ===
-            myRpc.propertyCollectionName &&
-          r.relationType === myRpc.relationType
-      )
-    }
-    if (!rco) rco = {}
-    rpc.push({
-      count: rco.count,
-      jsontype: 'String',
-      propertyCollectionName: myRpc.propertyCollectionName,
-      propertyName: 'Beziehungspartner_id',
-      relationType: myRpc.relationType,
-    })
-    rpc.push({
-      count: rco.count,
-      jsontype: 'String',
-      propertyCollectionName: myRpc.propertyCollectionName,
-      propertyName: 'Beziehungspartner_Name',
-      relationType: myRpc.relationType,
-    })
-  })
-  const rcoPropertiesFields = groupBy(rcoProperties, 'propertyName')
-  //console.log('Properties: pcoPropertiesFields:', pcoPropertiesFields)
-  /*console.log(
-    'Properties: rcoPropertiesByPropertyCollection:',
-    rcoPropertiesByPropertyCollection
-  )*/
-  const rCCount = Object.keys(rcoPropertiesByPropertyCollection).length
-
-  return (
-    <ErrorBoundary>
-      <Container>
-        <HowTo />
-        <Taxonomies
-          taxonomiesExpanded={taxonomiesExpanded}
-          onToggleTaxonomies={onToggleTaxonomies}
-        />
-        <PCOs pcoExpanded={pcoExpanded} onTogglePco={onTogglePco} />
-        <Level2Card expanded={rcoExpanded} onExpandChange={onToggleRco}>
-          <Level2CardHeader
-            title={
-              <div>
-                Beziehungssammlungen{rCCount > 0 && (
-                  <Level2Count>{`(${rCCount} Sammlungen, ${
-                    Object.keys(rcoPropertiesFields).length
-                  } ${
-                    Object.keys(rcoPropertiesFields).length === 1
-                      ? 'Feld'
-                      : 'Felder'
-                  })`}</Level2Count>
-                )}
-              </div>
-            }
-            actAsExpander={true}
-            showExpandableButton={true}
-            titleStyle={level2CardTitleStyle}
-          />
-          <Level2CardText expandable={true}>
-            {Object.keys(rcoPropertiesByPropertyCollection).map(pc => (
-              <Level3Card key={pc}>
-                <Level3CardHeader
-                  title={
-                    <div>
-                      {pc}
-                      <Level3Count>{`(${
-                        rcoPropertiesByPropertyCollection[pc].length
-                      } ${
-                        rcoPropertiesByPropertyCollection[pc].length === 1
-                          ? 'Feld'
-                          : 'Felder'
-                      })`}</Level3Count>
-                    </div>
-                  }
-                  actAsExpander={true}
-                  showExpandableButton={true}
-                  titleStyle={level2CardTitleStyle}
-                />
-                <Level3CardText expandable={true}>
-                  {rcoPropertiesByPropertyCollection[pc].length > 1 && (
-                    <AllRcoChooser
-                      properties={rcoPropertiesByPropertyCollection[pc]}
-                    />
-                  )}
-                  <PropertiesContainer data-width={window.innerWidth - 84}>
-                    {rcoPropertiesByPropertyCollection[pc].map(field => (
-                      <RcoChooser
-                        key={`${field.propertyName}${field.jsontype}`}
-                        pcname={field.propertyCollectionName}
-                        pname={field.propertyName}
-                        jsontype={field.jsontype}
-                        count={field.count}
-                      />
-                    ))}
-                  </PropertiesContainer>
-                </Level3CardText>
-              </Level3Card>
-            ))}
-          </Level2CardText>
-        </Level2Card>
-      </Container>
-    </ErrorBoundary>
-  )
-}
+}) => (
+  <ErrorBoundary>
+    <Container>
+      <HowTo />
+      <Taxonomies
+        taxonomiesExpanded={taxonomiesExpanded}
+        onToggleTaxonomies={onToggleTaxonomies}
+      />
+      <PCOs pcoExpanded={pcoExpanded} onTogglePco={onTogglePco} />
+      <RCOs rcoExpanded={rcoExpanded} onToggleRco={onToggleRco} />
+    </Container>
+  </ErrorBoundary>
+)
 
 export default enhance(Properties)
