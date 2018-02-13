5a87856cd60f558745346a1009681a2927b841c7
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-13 11:09:51 +0100

    modularize

--- src/components/Export/ChooseColumn/Properties/RCOs/RCO/index.js
+++ src/components/Export/ChooseColumn/Properties/RCOs/RCO/index.js
@@ -7,26 +7,33 @@ import get from 'lodash/get'
 import groupBy from 'lodash/groupBy'
 import compose from 'recompose/compose'
 
-import RCO from './RCO'
-import propsByTaxData from '../../propsByTaxData'
-import exportTaxonomiesData from '../../../exportTaxonomiesData'
-import data from '../data'
-import ErrorBoundary from '../../../../shared/ErrorBoundary'
+import AllRcoChooser from '../../AllRcoChooser'
+import RcoChooser from '../../RcoChooser'
+import constants from '../../../../../../modules/constants'
+import propsByTaxData from '../../../propsByTaxData'
+import exportTaxonomiesData from '../../../../exportTaxonomiesData'
+import data from '../../data'
+import ErrorBoundary from '../../../../../shared/ErrorBoundary'
 
-const Level2Card = styled(Card)`
-  margin: 10px 0;
+const StyledCard = styled(Card)`
+  margin: 0;
   padding: 0;
-  > div {
-    padding-bottom: 0 !important;
-  }
 `
-const Level2CardHeader = styled(CardHeader)`
-  background-color: #ffcc80;
+const StyledCardHeader = styled(CardHeader)`
+  background-color: #fff3e0;
+  border-bottom: 1px solid #ebebeb;
 `
-const Level2CardText = styled(CardText)`
-  padding: 0 !important;
+const StyledCardText = styled(CardText)`
+  display: flex;
+  flex-direction: column;
 `
-const Level2Count = styled.span`
+const PropertiesContainer = styled.div`
+  column-width: ${props =>
+    props['data-width'] > 2 * constants.export.properties.columnWidth
+      ? `${constants.export.properties.columnWidth}px`
+      : 'auto'};
+`
+const Count = styled.span`
   font-size: x-small;
   padding-left: 5px;
 `
@@ -35,16 +42,14 @@ const level2CardTitleStyle = { fontWeight: 'bold' }
 
 const enhance = compose(withApollo, exportTaxonomiesData, data, propsByTaxData)
 
-const RCOs = ({
+const RCO = ({
   propsByTaxData,
   data,
-  rcoExpanded,
-  onToggleRco,
+  pc,
 }: {
   propsByTaxData: Object,
   data: Object,
-  rcoExpanded: Boolean,
-  onToggleRco: () => {},
+  pc: Object,
 }) => {
   const rcoProperties = get(
     propsByTaxData,
@@ -99,38 +104,44 @@ const RCOs = ({
       relationType: myRpc.relationType,
     })
   })
-  const rcoPropertiesFields = groupBy(rcoProperties, 'propertyName')
-  const rCCount = Object.keys(rcoPropertiesByPropertyCollection).length
 
   return (
     <ErrorBoundary>
-      <Level2Card expanded={rcoExpanded} onExpandChange={onToggleRco}>
-        <Level2CardHeader
+      <StyledCard key={pc}>
+        <StyledCardHeader
           title={
             <div>
-              Beziehungssammlungen{rCCount > 0 && (
-                <Level2Count>{`(${rCCount} Sammlungen, ${
-                  Object.keys(rcoPropertiesFields).length
-                } ${
-                  Object.keys(rcoPropertiesFields).length === 1
-                    ? 'Feld'
-                    : 'Felder'
-                })`}</Level2Count>
-              )}
+              {pc}
+              <Count>{`(${rcoPropertiesByPropertyCollection[pc].length} ${
+                rcoPropertiesByPropertyCollection[pc].length === 1
+                  ? 'Feld'
+                  : 'Felder'
+              })`}</Count>
             </div>
           }
           actAsExpander={true}
           showExpandableButton={true}
           titleStyle={level2CardTitleStyle}
         />
-        <Level2CardText expandable={true}>
-          {Object.keys(rcoPropertiesByPropertyCollection).map(pc => (
-            <RCO key={pc} pc={pc} />
-          ))}
-        </Level2CardText>
-      </Level2Card>
+        <StyledCardText expandable={true}>
+          {rcoPropertiesByPropertyCollection[pc].length > 1 && (
+            <AllRcoChooser properties={rcoPropertiesByPropertyCollection[pc]} />
+          )}
+          <PropertiesContainer data-width={window.innerWidth - 84}>
+            {rcoPropertiesByPropertyCollection[pc].map(field => (
+              <RcoChooser
+                key={`${field.propertyName}${field.jsontype}`}
+                pcname={field.propertyCollectionName}
+                pname={field.propertyName}
+                jsontype={field.jsontype}
+                count={field.count}
+              />
+            ))}
+          </PropertiesContainer>
+        </StyledCardText>
+      </StyledCard>
     </ErrorBoundary>
   )
 }
 
-export default enhance(RCOs)
+export default enhance(RCO)
