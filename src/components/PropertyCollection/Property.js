8c55ff4508623313ebb56090852161df4c955fb8
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-14 11:23:16 +0100

    surface errors when saving

--- src/components/PropertyCollection/Property.js
+++ src/components/PropertyCollection/Property.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react'
 import TextField from 'material-ui/TextField'
+import { FormControl, FormHelperText } from 'material-ui/Form'
 import styled from 'styled-components'
 import compose from 'recompose/compose'
 import withHandlers from 'recompose/withHandlers'
@@ -14,19 +15,29 @@ import onBlur from './onBlur'
 const Container = styled.div`
   margin: 5px 0;
 `
+const StyledFormControl = styled(FormControl)`
+  width: 100%;
+`
+const StyledTextField = styled(TextField)`
+  p {
+    color: ${props => (props.error ? 'red' : 'rgba(0,0,0,0.54)')};
+  }
+`
 
 const enhance = compose(
   withApollo,
   withState('value', 'setValue', ({ pC, field }) => pC[field] || ''),
+  withState('error', 'setError', null),
   withHandlers({
     onChange: ({ setValue }) => event => setValue(event.target.value),
-    onBlur: ({ client, field, pC, value }) => event =>
+    onBlur: ({ client, field, pC, value, setError }) => event =>
       onBlur({
         client,
         field,
         pC,
         value: event.target.value,
         prevValue: pC[field],
+        setError,
       }),
   })
 )
@@ -36,6 +47,7 @@ const Property = ({
   field,
   label,
   value,
+  error,
   helperText,
   type = 'text',
   disabled,
@@ -47,34 +59,40 @@ const Property = ({
   label: String,
   disabled: Boolean,
   value: String,
+  error: String,
   helperText: Element,
   type: String,
   onChange: () => void,
   onBlur: () => void,
 }) => {
+  console.log('error:', error)
+  console.log('!!error:', !!error)
   return (
     <ErrorBoundary>
       <Container>
-        <TextField
-          autoFocus={label === 'Name' && !value}
-          label={label}
-          value={
-            field === 'lastUpdated' && value
-              ? format(new Date(value), 'DD.MM.YYYY')
-              : value
-          }
-          onChange={onChange}
-          onBlur={onBlur}
-          fullWidth
-          multiline
-          autoComplete="off"
-          autoCorrect="off"
-          autoCapitalize="off"
-          spellCheck="false"
-          disabled={!!disabled}
-          type={type}
-          helperText={helperText ? helperText : ''}
-        />
+        <StyledFormControl error={!!error}>
+          <StyledTextField
+            autoFocus={label === 'Name' && !value}
+            label={label}
+            value={
+              field === 'lastUpdated' && value
+                ? format(new Date(value), 'DD.MM.YYYY')
+                : value
+            }
+            onChange={onChange}
+            onBlur={onBlur}
+            fullWidth
+            multiline
+            autoComplete="off"
+            autoCorrect="off"
+            autoCapitalize="off"
+            spellCheck="false"
+            disabled={!!disabled}
+            type={type}
+            helperText={error ? error : helperText ? helperText : ''}
+            error={!!error}
+          />
+        </StyledFormControl>
       </Container>
     </ErrorBoundary>
   )
--- src/components/PropertyCollection/onBlur.js
+++ src/components/PropertyCollection/onBlur.js
@@ -2,10 +2,11 @@
 
 import updatePCMutation from './updatePCMutation'
 
-export default ({ client, field, pC, value, prevValue }) => {
+export default async ({ client, field, pC, value, prevValue, setError }) => {
+  setError(null)
   if (value !== prevValue) {
     const variables = {
-      id: pC.id,
+      id: field === 'id' ? value : pC.id,
       name: field === 'name' ? value : pC.name,
       description: field === 'description' ? value : pC.description,
       links: field === 'links' ? value.split(',') : pC.links,
@@ -15,28 +16,33 @@ export default ({ client, field, pC, value, prevValue }) => {
       importedBy: field === 'importedBy' ? value : pC.importedBy,
       termsOfUse: field === 'termsOfUse' ? value : pC.termsOfUse,
     }
-    client.mutate({
-      mutation: updatePCMutation,
-      variables,
-      optimisticResponse: {
-        updatePropertyCollectionById: {
-          propertyCollection: {
-            id: pC.id,
-            name: field === 'name' ? value : pC.name,
-            description: field === 'description' ? value : pC.description,
-            links: field === 'links' ? value.split(',') : pC.links,
-            combining: field === 'combining' ? value : pC.combining,
-            organizationId:
-              field === 'organizationId' ? value : pC.organizationId,
-            lastUpdated: field === 'lastUpdated' ? value : pC.lastUpdated,
-            importedBy: field === 'importedBy' ? value : pC.importedBy,
-            termsOfUse: field === 'termsOfUse' ? value : pC.termsOfUse,
+    try {
+      await client.mutate({
+        mutation: updatePCMutation,
+        variables,
+        optimisticResponse: {
+          updatePropertyCollectionById: {
+            propertyCollection: {
+              id: pC.id,
+              name: field === 'name' ? value : pC.name,
+              description: field === 'description' ? value : pC.description,
+              links: field === 'links' ? value.split(',') : pC.links,
+              combining: field === 'combining' ? value : pC.combining,
+              organizationId:
+                field === 'organizationId' ? value : pC.organizationId,
+              lastUpdated: field === 'lastUpdated' ? value : pC.lastUpdated,
+              importedBy: field === 'importedBy' ? value : pC.importedBy,
+              termsOfUse: field === 'termsOfUse' ? value : pC.termsOfUse,
+              __typename: 'PropertyCollection',
+            },
             __typename: 'PropertyCollection',
           },
-          __typename: 'PropertyCollection',
+          __typename: 'Mutation',
         },
-        __typename: 'Mutation',
-      },
-    })
+      })
+    } catch (error) {
+      console.log(error.message)
+      return setError(error.message)
+    }
   }
 }
