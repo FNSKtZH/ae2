b334a85771c0da42731172a2aad73816512a99e1
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-26 22:09:47 +0100

    detect whether layout should be stacked

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 import compose from 'recompose/compose'
 import Reboot from 'material-ui/Reboot'
@@ -36,45 +36,84 @@ const FourOhFourAsync = Loadable({
 
 const enhance = compose(activeNodeArrayData)
 
-const App = ({ activeNodeArrayData }: { activeNodeArrayData: Object }) => {
-  const activeNodeArray = get(activeNodeArrayData, 'activeNodeArray', [])
-  const url0 =
-    activeNodeArray[0] && activeNodeArray[0].toLowerCase()
-      ? activeNodeArray[0].toLowerCase()
-      : null
-  const show404 = ![
-    null,
-    'arten',
-    'lebensräume',
-    'eigenschaften-sammlungen',
-    'organisationen',
-    'export',
-    'login',
-    'benutzer',
-  ].includes(url0)
-  const showData = [
-    null,
-    'arten',
-    'lebensräume',
-    'eigenschaften-sammlungen',
-    'benutzer',
-    'organisationen',
-  ].includes(url0)
-  const showExport = url0 === 'export'
-  const showLogin = url0 === 'login'
+type Props = {
+  activeNodeArrayData: Object,
+}
 
-  return (
-    <ErrorBoundary>
-      <Container>
-        <Reboot />
-        <AppBar />
-        {showData && <DataAsync />}
-        {showExport && <ExportAsync />}
-        {showLogin && <LoginAsync />}
-        {show404 && <FourOhFourAsync />}
-      </Container>
-    </ErrorBoundary>
-  )
+type State = {
+  stacked: Boolean,
+}
+class App extends Component<Props, State> {
+  state = {
+    stacked: false,
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateStacked)
+    setTimeout(() => this.updateStacked())
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateStacked)
+  }
+
+  updateStacked = () => {
+    const { stacked } = this.state
+    const w = window
+    const d = document
+    const e = d.documentElement
+    const g = d.getElementsByTagName('body')[0]
+    const windowWidth = w.innerWidth || e.clientWidth || g.clientWidth
+    console.log('windowWidth:', windowWidth)
+    const shouldBeStacked = windowWidth < 500
+    if (shouldBeStacked !== stacked) {
+      this.setState({ stacked: shouldBeStacked })
+    }
+  }
+
+  render() {
+    const { activeNodeArrayData } = this.props
+    const { stacked } = this.state
+    const activeNodeArray = get(activeNodeArrayData, 'activeNodeArray', [])
+    const url0 =
+      activeNodeArray[0] && activeNodeArray[0].toLowerCase()
+        ? activeNodeArray[0].toLowerCase()
+        : null
+    const show404 = ![
+      null,
+      'arten',
+      'lebensräume',
+      'eigenschaften-sammlungen',
+      'organisationen',
+      'export',
+      'login',
+      'benutzer',
+    ].includes(url0)
+    const showData = [
+      null,
+      'arten',
+      'lebensräume',
+      'eigenschaften-sammlungen',
+      'benutzer',
+      'organisationen',
+    ].includes(url0)
+    const showExport = url0 === 'export'
+    const showLogin = url0 === 'login'
+    console.log('stacked:', stacked)
+
+    return (
+      <ErrorBoundary>
+        <Container>
+          <Reboot />
+          <AppBar />
+          {showData && <DataAsync />}
+          {showExport && <ExportAsync />}
+          {showLogin && <LoginAsync />}
+          {show404 && <FourOhFourAsync />}
+        </Container>
+      </ErrorBoundary>
+    )
+  }
 }
 
 export default enhance(App)
