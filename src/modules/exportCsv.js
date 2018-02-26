8e4482d822409ddcb3732e0ed4ff7e3c33d2c70c
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-26 09:17:26 +0100

    Revert "update json2csv"
    This reverts commit 320675a88815751a6da0c5b65bd53d37b1faf267.

--- src/modules/exportCsv.js
+++ src/modules/exportCsv.js
@@ -1,10 +1,10 @@
 // @flow
-import { parse } from 'json2csv'
+import json2csv from 'json2csv'
 import fileDownload from 'js-file-download'
 import format from 'date-fns/format'
 
 export default (jsonData: Array<Object>) => {
-  const csvData = parse(jsonData)
+  const csvData = json2csv({ data: jsonData })
   fileDownload(
     csvData,
     `arteigenschaften_${format(new Date(), 'YYYY-MM-DD_HH-mm-ss')}.csv`
