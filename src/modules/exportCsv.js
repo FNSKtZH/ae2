c944993f2663fbabacd4173f0b51ae3e3c5a1000
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-28 11:00:40 +0100

    update json2csv

--- src/modules/exportCsv.js
+++ src/modules/exportCsv.js
@@ -1,10 +1,10 @@
 // @flow
-import json2csv from 'json2csv'
+import { parse } from 'json2csv'
 import fileDownload from 'js-file-download'
 import format from 'date-fns/format'
 
 export default (jsonData: Array<Object>) => {
-  const csvData = json2csv({ data: jsonData })
+  const csvData = parse(jsonData)
   fileDownload(
     csvData,
     `arteigenschaften_${format(new Date(), 'YYYY-MM-DD_HH-mm-ss')}.csv`
