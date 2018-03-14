a854e114c8d9711c177deb1c8b83ffb70554c6d6
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-14 17:24:49 +0100

    simplify

--- src/components/Benutzer/userData.js
+++ src/components/Benutzer/userData.js
@@ -1,16 +1,47 @@
 // @flow
 import { graphql } from 'react-apollo'
 import get from 'lodash/get'
+import gql from 'graphql-tag'
 
-import userGql from './userGql'
-
-export default graphql(userGql, {
-  options: ({ activeNodeArrayData }) => ({
-    variables: {
-      id:
-        get(activeNodeArrayData, 'activeNodeArray', [])[1] ||
-        '99999999-9999-9999-9999-999999999999',
-    },
-  }),
-  name: 'userData',
-})
+export default graphql(
+  gql`
+    query userQuery($id: UUID!) {
+      userById(id: $id) {
+        id
+        name
+        email
+        organizationUsersByUserId {
+          nodes {
+            organizationByOrganizationId {
+              id
+              name
+            }
+            role
+          }
+        }
+        propertyCollectionsByImportedBy {
+          nodes {
+            id
+            name
+          }
+        }
+        taxonomiesByImportedBy {
+          nodes {
+            id
+            name
+          }
+        }
+      }
+    }
+  `,
+  {
+    options: ({ activeNodeArrayData }) => ({
+      variables: {
+        id:
+          get(activeNodeArrayData, 'activeNodeArray', [])[1] ||
+          '99999999-9999-9999-9999-999999999999',
+      },
+    }),
+    name: 'userData',
+  }
+)
