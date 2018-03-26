be92f284684ac5cab2a8719f7e619195ef2610f3
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-26 15:45:26 +0200

    classify

--- src/components/AltGenerateUrl/ChooseColumn/index.js
+++ src/components/AltGenerateUrl/ChooseColumn/index.js
@@ -1,17 +1,15 @@
 // @flow
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 import { withApollo } from 'react-apollo'
 import compose from 'recompose/compose'
 import withState from 'recompose/withState'
 import withHandlers from 'recompose/withHandlers'
-import get from 'lodash/get'
 
 import HowTo from './HowTo'
 import Taxonomies from './Taxonomies'
 import PCOs from './PCOs'
 import RCOs from './RCOs'
-import propsByTaxData from './propsByTaxData'
 import exportTaxonomiesData from '../exportTaxonomiesData'
 import data from './data'
 import ErrorBoundary from '../../shared/ErrorBoundary'
@@ -35,7 +33,6 @@ const enhance = compose(
   withApollo,
   exportTaxonomiesData,
   data,
-  propsByTaxData,
   withState('taxonomiesExpanded', 'setTaxonomiesExpanded', false),
   withState('pcoExpanded', 'setFilterExpanded', false),
   withState('rcoExpanded', 'setPropertiesExpanded', false),
@@ -95,60 +92,46 @@ const enhance = compose(
       }
     },
   })
-  //withWindowSize,
 )
+class Properties extends Component {
+  props: {
+    data: Object,
+    taxonomiesExpanded: Boolean,
+    pcoExpanded: Boolean,
+    rcoExpanded: Boolean,
+    onToggleTaxonomies: () => {},
+    onTogglePco: () => {},
+    onToggleRco: () => {},
+    message: String,
+  }
 
-const Properties = ({
-  propsByTaxData,
-  data,
-  taxonomiesExpanded,
-  pcoExpanded,
-  rcoExpanded,
-  onToggleTaxonomies,
-  onTogglePco,
-  onToggleRco,
-  message,
-}: {
-  propsByTaxData: Object,
-  data: Object,
-  taxonomiesExpanded: Boolean,
-  pcoExpanded: Boolean,
-  rcoExpanded: Boolean,
-  onToggleTaxonomies: () => {},
-  onTogglePco: () => {},
-  onToggleRco: () => {},
-  message: String,
-}) => {
-  const pcoProperties = get(
-    propsByTaxData,
-    'pcoPropertiesByTaxonomiesFunction.nodes',
-    []
-  )
-  const rcoProperties = get(
-    propsByTaxData,
-    'rcoPropertiesByTaxonomiesFunction.nodes',
-    []
-  )
+  render() {
+    const {
+      taxonomiesExpanded,
+      pcoExpanded,
+      rcoExpanded,
+      onToggleTaxonomies,
+      onTogglePco,
+      onToggleRco,
+      message,
+    } = this.props
 
-  return (
-    <ErrorBoundary>
-      <Container>
-        <StyledH3>Eigenschaften wählen</StyledH3>
-        <HowTo />
-        <Taxonomies
-          taxonomiesExpanded={taxonomiesExpanded}
-          onToggleTaxonomies={onToggleTaxonomies}
-        />
-        {pcoProperties.length > 0 && (
+    return (
+      <ErrorBoundary>
+        <Container>
+          <StyledH3>Eigenschaften wählen</StyledH3>
+          <HowTo />
+          <Taxonomies
+            taxonomiesExpanded={taxonomiesExpanded}
+            onToggleTaxonomies={onToggleTaxonomies}
+          />
           <PCOs pcoExpanded={pcoExpanded} onTogglePco={onTogglePco} />
-        )}
-        {rcoProperties.length > 0 && (
           <RCOs rcoExpanded={rcoExpanded} onToggleRco={onToggleRco} />
-        )}
-        <StyledSnackbar open={!!message} message={message} />
-      </Container>
-    </ErrorBoundary>
-  )
+          <StyledSnackbar open={!!message} message={message} />
+        </Container>
+      </ErrorBoundary>
+    )
+  }
 }
 
 export default enhance(Properties)
