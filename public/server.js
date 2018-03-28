ba43635417667b7abc2d0e741b162de39765758c
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-28 13:06:01 +0200

    configure routes after inert is loaded

--- public/server.js
+++ public/server.js
@@ -13,105 +13,96 @@ const serverOptionsDevelopment = {
     request: ['error']
   }
 }*/
+
 const server = new Hapi.Server({
   host: '0.0.0.0',
   port: 3000,
 })
 
-server.route({
-  method: 'GET',
-  path: '/manifest.json',
-  handler: {
-    file: 'manifest.json',
-  },
-})
-
-server.route({
-  method: 'GET',
-  path: '/asset-manifest.json',
-  handler: {
-    file: 'asset-manifest.json',
-  },
-})
-
-server.route({
-  method: 'GET',
-  path: '/service-worker.js',
-  handler: {
-    file: 'service-worker.js',
-  },
-})
-
-server.route({
-  method: 'GET',
-  path: '/favicon.ico',
-  handler: {
-    file: 'favicon.ico',
-  },
-})
-
-server.route({
-  method: 'GET',
-  path: '/{path*}',
-  handler: {
-    file: 'index.html',
-  },
-})
-
-server.route({
-  method: 'GET',
-  path: '/static/css/{param*}',
-  handler: {
-    directory: {
-      path: 'static/css',
-      index: false,
-    },
-  },
-})
-
-server.route({
-  method: 'GET',
-  path: '/static/js/{param*}',
-  handler: {
-    directory: {
-      path: 'static/js',
-      index: false,
-    },
-  },
-})
-
-server.route({
-  method: 'GET',
-  path: '/static/media/{param*}',
-  handler: {
-    directory: {
-      path: 'static/media',
-      index: false,
-    },
-  },
-})
-
-server.route({
-  method: 'GET',
-  path: '/favicons/{param*}',
-  handler: {
-    directory: {
-      path: 'favicons',
-      index: false,
-    },
-  },
-})
-
-server.route({
-  method: 'GET',
-  path:
-    'artendb/_design/artendb/_list/export_alt_mit_synonymen_standardfelder/alt_arten_mit_synonymen?include_docs=true',
-  handler: () =>
-    'hello, you reached artendb/_design/artendb/_list/export_alt_mit_synonymen_standardfelder/alt_arten_mit_synonymen?include_docs=true',
-})
-
 async function start() {
   await server.register(Inert)
+
+  server.route({
+    method: 'GET',
+    path: '/manifest.json',
+    handler: {
+      file: 'manifest.json',
+    },
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/asset-manifest.json',
+    handler: {
+      file: 'asset-manifest.json',
+    },
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/service-worker.js',
+    handler: {
+      file: 'service-worker.js',
+    },
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/favicon.ico',
+    handler: {
+      file: 'favicon.ico',
+    },
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: (request, h) => h.file('index.html'),
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/static/css/{param*}',
+    handler: {
+      directory: {
+        path: 'static/css',
+        index: false,
+      },
+    },
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/static/js/{param*}',
+    handler: {
+      directory: {
+        path: 'static/js',
+        index: false,
+      },
+    },
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/static/media/{param*}',
+    handler: {
+      directory: {
+        path: 'static/media',
+        index: false,
+      },
+    },
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/favicons/{param*}',
+    handler: {
+      directory: {
+        path: 'favicons',
+        index: false,
+      },
+    },
+  })
   await server.start()
   console.log('Server running at:', server.info.uri)
 }
