b9bc906f2f7d9538dd9de918aa99ffbb7dd2fa44
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-28 23:46:53 +0200

    comment error

--- src/modules/initializeIdb.js
+++ src/modules/initializeIdb.js
@@ -4,6 +4,8 @@ import Dexie from 'dexie'
 export default () => {
   // create table to save user login in
   // this helps in that user can open new tab and remain logged in!
+  // seems that this creates error in old versions of firefox (before 55)
+  // maybe because apollo creates own indexedDB right after this?
   const idb = new Dexie('ae')
   idb.version(2).stores({
     users: '++id,token,username',
