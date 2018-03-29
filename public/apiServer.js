73e83ab96fe7c52abecafa4d13478a9e6378256d
Alexander Gabriel <alex.barbalex@gmail.com>
2018-03-29 11:03:35 +0200

    correct error

--- public/apiServer.js
+++ public/apiServer.js
@@ -28,15 +28,15 @@ async function start() {
       return 'hello, you reached "artendb/_design/artendb/_list/export_alt_mit_synonymen_standardfelder/alt_arten_mit_synonymen"'
     },
   })
-  async function start() {
-    server.route({
-      method: 'GET',
-      path:
-        '/artendb/_design/artendb/_list/export_alt_mit_synonymen/alt_arten_mit_synonymen',
-      handler: (request, h) => {
-        return 'hello, you reached "artendb/_design/artendb/_list/export_alt_mit_synonymen/alt_arten_mit_synonymen"'
-      },
-    })
+
+  server.route({
+    method: 'GET',
+    path:
+      '/artendb/_design/artendb/_list/export_alt_mit_synonymen/alt_arten_mit_synonymen',
+    handler: (request, h) => {
+      return 'hello, you reached "artendb/_design/artendb/_list/export_alt_mit_synonymen/alt_arten_mit_synonymen"'
+    },
+  })
 
   server.route({
     method: 'GET',
