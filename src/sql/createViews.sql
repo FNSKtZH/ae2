8d659a74f7c83dad5f3436e3c5e23ee4cecd472a
Alexander Gabriel <alex.barbalex@gmail.com>
2018-04-01 21:24:14 +0200

    add ae.evab_arten

--- src/sql/createViews.sql
+++ src/sql/createViews.sql
@@ -91,16 +91,53 @@ where
   ae.user.name = current_user_name()
   and ae.organization_user.role in ('orgCollectionWriter', 'orgAdmin');
 
-create or replace view ae.evab_arten_fauna as
+create or replace view ae.evab_arten as
 select
   concat('{', upper(ae.object.id::TEXT), '}') as "idArt",
   ae.object.properties->>'Taxonomie ID' as "nummer",
   substring(COALESCE(ae.object.properties->>'Artname', concat(ae.object.properties->>'Gattung', ' ', ae.object.properties->>'Art')), 1, 255) as "wissenschArtname",
-  substring(ae.object.properties->>'Name Deutsch', 1, 255) as "deutscherArtname"
+  substring(ae.object.properties->>'Name Deutsch', 1, 255) as "deutscherArtname",
+  'A'::text as status,
+  substring(ae.property_collection_object.properties->>'GIS-Layer', 1, 50) as "klasse"
+from
+  ae.object
+  inner join ae.taxonomy
+  on ae.object.taxonomy_id = ae.taxonomy.id
+  left join ae.property_collection_object
+  on ae.object.id = ae.property_collection_object.object_id
+    inner join ae.property_collection
+    on ae.property_collection_object.property_collection_id = ae.property_collection.id
+where
+  ae.taxonomy.name = 'CSCF (2009)'
+  and ae.object.properties is not null
+  and ae.property_collection.name = 'ZH GIS'
+UNION select
+  concat('{', upper(ae.object.id::TEXT), '}') as "idArt",
+  ae.object.properties->>'Taxonomie ID' as "nummer",
+  substring(COALESCE(ae.object.properties->>'Artname', concat(ae.object.properties->>'Gattung', ' ', ae.object.properties->>'Art')), 1, 255) as "wissenschArtname",
+  substring(ae.object.properties->>'Name Deutsch', 1, 255) as "deutscherArtname",
+  ae.evab_flora_status.encoded as status,
+  'Flora'::text as "klasse"
+from
+  ae.object
+  inner join ae.taxonomy
+  on ae.object.taxonomy_id = ae.taxonomy.id
+  left join ae.evab_flora_status
+  on ae.object.properties->>'Status' = ae.evab_flora_status.decoded
+where
+  ae.taxonomy.name = 'SISF Index 2 (2005)'
+  and ae.object.properties is not null
+UNION select
+  concat('{', upper(ae.object.id::TEXT), '}') as "idArt",
+  ae.object.properties->>'Taxonomie ID' as "nummer",
+  substring(COALESCE(ae.object.properties->>'Artname', concat(ae.object.properties->>'Gattung', ' ', ae.object.properties->>'Art')), 1, 255) as "wissenschArtname",
+  substring(ae.object.properties->>'Name Deutsch', 1, 255) as "deutscherArtname",
+  'A'::text as status,
+  'Moose'::text as "klasse"
 from
   ae.object
   inner join ae.taxonomy
   on ae.object.taxonomy_id = ae.taxonomy.id
 where
-  ae.taxonomy.name = 'CSCF (2009)'
-  and ae.object.properties is not null
+  ae.taxonomy.name = 'SISF Index 2 (2005)'
+  and ae.object.properties is not null;
