2b3a163a6accb6d30e867bbb1522202bf8c885cd
Alexander Gabriel <alex.barbalex@gmail.com>
2018-04-02 12:37:33 +0200

    work on view alt_standard

--- src/sql/createViews.sql
+++ src/sql/createViews.sql
@@ -141,3 +141,95 @@ from
 where
   ae.taxonomy.name = 'NISM (2010)'
   and ae.object.properties is not null;
+
+create or replace view ae.alt_standard as
+select
+  concat('{', upper(ae.object.id::TEXT), '}') as "idArt",
+  ae.object.properties->>'Taxonomie ID' as "ref",
+  substring(ae.property_collection_object.properties->>'GIS-Layer', 1, 50) as "gisLayer",
+  (ae.property_collection_object.properties->>'Betrachtungsdistanz (m)')::integer AS "distance",
+  substring(COALESCE(ae.object.properties->>'Artname', concat(ae.object.properties->>'Gattung', ' ', ae.object.properties->>'Art'), '(kein Artname)'), 1, 255) as "nameLat",
+  substring(ae.object.properties->>'Name Deutsch', 1, 255) as "nameDeu"
+from
+  ae.object
+  inner join ae.taxonomy
+  on ae.object.taxonomy_id = ae.taxonomy.id
+    inner join ae.property_collection_object
+    on ae.property_collection_object.object_id = ae.object.id
+      inner join ae.property_collection
+      on ae.property_collection_object.property_collection_id = ae.property_collection.id
+where
+  ae.taxonomy.name in('CSCF (2009)', 'SISF Index 2 (2005)')
+  -- removes all structural nodes not included in sisf2
+  and ae.object.properties is not null
+  and ae.object.properties->>'Taxonomie ID' is not null
+  -- beware: every object needs an entry in ZH GIS...
+  and ae.property_collection.name = 'ZH GIS'
+  -- ...with GIS-Layer and Betrachtungsdistanz!
+  and ae.property_collection_object.properties->>'GIS-Layer' is not null
+  and ae.property_collection_object.properties->>'Betrachtungsdistanz (m)' is not null;
+
+
+
+
+create or replace view ae.alt_maybe_use_later as
+select
+  concat('{', upper(ae.object.id::TEXT), '}') as "idArt",
+  ae.object.properties->>'Taxonomie ID' as "nummer",
+  CASE
+    WHEN EXISTS(
+      SELECT
+        ae.property_collection_object.properties->>'Betrachtungsdistanz (m)'
+      FROM
+        ae.property_collection_object
+        inner join ae.property_collection
+        on ae.property_collection_object.property_collection_id = ae.property_collection.id
+      WHERE
+        ae.property_collection_object.object_id = ae.object.id
+        and ae.property_collection.name = 'ZH GIS'
+        and ae.property_collection_object.properties->>'Betrachtungsdistanz (m)' is not null
+    ) THEN (
+      SELECT
+        ae.property_collection_object.properties->>'Betrachtungsdistanz (m)'
+      FROM
+        ae.property_collection_object
+        inner join ae.property_collection
+        on ae.property_collection_object.property_collection_id = ae.property_collection.id
+      WHERE
+        ae.property_collection_object.object_id = ae.object.id
+        and ae.property_collection.name = 'ZH GIS'
+    )
+    WHEN EXISTS(
+      SELECT
+        ae.property_collection_object.properties->>'Betrachtungsdistanz (m)'
+      FROM
+        ae.property_collection_object
+        inner join ae.property_collection
+        on ae.property_collection_object.property_collection_id = ae.property_collection.id
+      WHERE
+        ae.property_collection_object.object_id = ae.object.id
+        and ae.property_collection.name = 'ZH GIS'
+    ) THEN (
+      SELECT
+        ae.property_collection_object.properties->>'Betrachtungsdistanz (m)'
+      FROM
+        ae.property_collection_object
+        inner join ae.property_collection
+        on ae.property_collection_object.property_collection_id = ae.property_collection.id
+      WHERE
+        ae.property_collection_object.object_id in (select object_id_synonym from ae.synonym where object_id = ae.object.id)
+        and ae.property_collection_object.object_id = ae.object.id
+        and ae.property_collection.name = 'ZH GIS'
+        and ae.property_collection_object.properties->>'Betrachtungsdistanz (m)' is not null
+    )
+    ELSE null
+  END AS "Betrachtungsdistanz (m)"
+from
+  ae.object
+  inner join ae.taxonomy
+  on ae.object.taxonomy_id = ae.taxonomy.id
+where
+  ae.taxonomy.name in('CSCF (2009)', 'SISF Index 2 (2005)')
+  and ae.object.properties is not null
+  and ae.object.properties->>'Taxonomie ID' is not null
+  and 'Betrachtungsdistanz (m)' is not null;
