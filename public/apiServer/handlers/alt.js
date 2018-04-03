07562041dbb5d22b64b0cb1277a6a5592fc0223e
Alexander Gabriel <alex.barbalex@gmail.com>
2018-04-03 15:56:36 +0200

    work on alt

--- public/apiServer/handlers/alt.js
+++ public/apiServer/handlers/alt.js
@@ -2,11 +2,7 @@
 const app = require(`ampersand-app`)
 
 module.exports = async (request, h) => {
-  //console.log('request:', request)
   console.log('request.path:', request.path)
-  console.log('request.params:', request.params)
-  console.log('request.paramsArray:', request.paramsArray)
-  console.log('request.payload:', request.payload)
-  //console.log('h:', h)
+  console.log('request.query:', request.query)
   return await app.db.any('select * from ae.alt_standard')
 }
