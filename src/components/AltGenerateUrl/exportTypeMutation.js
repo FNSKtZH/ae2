4fcc14be3f366a6d4ca786746a024f721e50886d
Alexander Gabriel <alex.barbalex@gmail.com>
2018-06-29 11:10:35 +0200

    remove unused

--- src/components/AltGenerateUrl/exportTypeMutation.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import gql from 'graphql-tag'
-
-export default gql`
-  mutation setExportType($value: String) {
-    setExportType(value: $value) @client
-  }
-`
--- src/components/AltGenerateUrl/exportWithSynonymDataMutation.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import gql from 'graphql-tag'
-
-export default gql`
-  mutation setExportWithSynonymData($value: Array) {
-    setExportWithSynonymData(value: $value) @client
-  }
-`
