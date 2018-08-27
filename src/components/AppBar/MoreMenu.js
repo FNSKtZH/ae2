5d9af447375ba09438f763870e31c7be3b677d4f
Alexander Gabriel <alex.barbalex@gmail.com>
2018-08-27 23:02:32 +0200

    update deps

--- src/components/AppBar/MoreMenu.js
+++ src/components/AppBar/MoreMenu.js
@@ -104,7 +104,7 @@ class MoreMenu extends React.Component {
           >
             Felder für das Artenlistentool wählen
           </MenuItem>
-          <Version>Version: 1.0.2 vom 3.7.2018</Version>
+          <Version>Version: 1.0.3 vom 27.8.2018</Version>
           {/*<MenuItem
             key="datagraph"
             onClick={() => {
