54eed0ae2c221ecfa732bd0627c76fe82219f136
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2018-12-29 12:05:03 +0100

    optimize

--- src/components/Tree/contextmenu/BenutzerFolder.js
+++ src/components/Tree/contextmenu/BenutzerFolder.js
@@ -2,19 +2,18 @@
 import React from 'react'
 import { ContextMenu, MenuItem, connectMenu } from 'react-contextmenu'
 
+const data = {
+  action: 'insert',
+  table: 'user',
+}
+
 const DynamicMenu = ({ id, trigger }) => {
   const handleItemClick = trigger ? trigger.onItemClick : null
 
   return (
     <ContextMenu id={id} collect={props => props}>
       <div className="react-contextmenu-title">Benutzer</div>
-      <MenuItem
-        onClick={handleItemClick}
-        data={{
-          action: 'insert',
-          table: 'user',
-        }}
-      >
+      <MenuItem onClick={handleItemClick} data={data}>
         erstelle neuen
       </MenuItem>
     </ContextMenu>
