0a11d77956aaf82b81b056decd35f29203de08a0
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2018-12-29 12:05:09 +0100

    optimize

--- src/components/Tree/contextmenu/PC.js
+++ src/components/Tree/contextmenu/PC.js
@@ -2,6 +2,11 @@
 import React from 'react'
 import { ContextMenu, MenuItem, connectMenu } from 'react-contextmenu'
 
+const dataDelete = {
+  action: 'delete',
+  table: 'pc',
+}
+
 const DynamicMenu = ({ id, trigger }) => {
   const handleItemClick = trigger ? trigger.onItemClick : null
   const nodeLabel = trigger ? trigger.nodeLabel : ''
@@ -9,13 +14,7 @@ const DynamicMenu = ({ id, trigger }) => {
   return (
     <ContextMenu id={id} collect={props => props}>
       <div className="react-contextmenu-title">{nodeLabel}</div>
-      <MenuItem
-        onClick={handleItemClick}
-        data={{
-          action: 'delete',
-          table: 'pc',
-        }}
-      >
+      <MenuItem onClick={handleItemClick} data={dataDelete}>
         lösche
       </MenuItem>
     </ContextMenu>
--- src/components/Tree/contextmenu/Type.js
+++ src/components/Tree/contextmenu/Type.js
@@ -2,6 +2,11 @@
 import React from 'react'
 import { ContextMenu, MenuItem, connectMenu } from 'react-contextmenu'
 
+const dataInsert = {
+  action: 'insert',
+  table: 'taxonomy',
+}
+
 const DynamicMenu = ({ id, trigger }) => {
   const handleItemClick = trigger ? trigger.onItemClick : null
   const nodeLabel = trigger ? trigger.nodeLabel : ''
@@ -9,13 +14,7 @@ const DynamicMenu = ({ id, trigger }) => {
   return (
     <ContextMenu id={id} collect={props => props}>
       <div className="react-contextmenu-title">{nodeLabel}</div>
-      <MenuItem
-        onClick={handleItemClick}
-        data={{
-          action: 'insert',
-          table: 'taxonomy',
-        }}
-      >
+      <MenuItem onClick={handleItemClick} data={dataInsert}>
         erstelle neue Taxonomie
       </MenuItem>
     </ContextMenu>
