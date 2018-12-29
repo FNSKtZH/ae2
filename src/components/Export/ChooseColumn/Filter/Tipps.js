95f6915a519acfbe84113b81a450d9edee8b5842
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2018-12-29 12:55:21 +0100

    use hooks

--- src/components/Export/ChooseColumn/Filter/Tipps.js
+++ src/components/Export/ChooseColumn/Filter/Tipps.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
@@ -7,8 +7,6 @@ import Collapse from '@material-ui/core/Collapse'
 import IconButton from '@material-ui/core/IconButton'
 import Icon from '@material-ui/core/Icon'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import compose from 'recompose/compose'
-import withState from 'recompose/withState'
 import styled from 'styled-components'
 
 const StyledCard = styled(Card)`
@@ -36,70 +34,66 @@ const StyledCardContent = styled(CardContent)`
   }
 `
 
-const enhance = compose(withState('expanded', 'setExpanded', false))
+const Tipps = () => {
+  const [expanded, setExpanded] = useState(false)
 
-const Tipps = ({
-  expanded,
-  setExpanded,
-}: {
-  expanded: Boolean,
-  setExpanded: () => void,
-}) => (
-  <StyledCard>
-    <StyledCardActions
-      disableActionSpacing
-      onClick={() => setExpanded(!expanded)}
-    >
-      <CardActionTitle>Tipps und Tricks</CardActionTitle>
-      <CardActionIconButton
-        data-expanded={expanded}
-        aria-expanded={expanded}
-        aria-label="Show more"
+  return (
+    <StyledCard>
+      <StyledCardActions
+        disableActionSpacing
+        onClick={() => setExpanded(!expanded)}
       >
-        <Icon>
-          <ExpandMoreIcon />
-        </Icon>
-      </CardActionIconButton>
-    </StyledCardActions>
-    <Collapse in={expanded} timeout="auto" unmountOnExit>
-      <StyledCardContent>
-        <ul>
-          <li>
-            {`Sie können nach beliebig vielen Eigenschaften filtern. Jedes Kriterium reduziert die Anzahl "Treffer".`}
-            <br />
-            {`Beispiel: Filtern Sie im Namen nach "Eisvogel" und in der Ordnung nach "Lepidoptera", erhalten Sie drei Schmetterlinge aber nicht den entsprechenden Vogel.`}
-          </li>
-          <li>
-            {`Sie möchten nach "Schmetterlinge" oder "Vögel" suchen? Exportieren Sie zuerst "Schmetterlinge", danach die "Vögel" und setzen die zwei Exporte zusammen.`}
-          </li>
-          <li>
-            {`Sie können Vergleichsoperatoren verwenden.`}
-            <br />
-            {`Das Feld, um sie zu wählen erscheint, nachdem ein Filter-Wert erfasst wurde.`}
-          </li>
-          <li>
-            {`Es kommt nicht auf Gross-/Kleinschreibung an.`}
-            <br />
-            {`Beispiel: Schreiben Sie "eisvogel", wird auch "Eisvogel" gefunden.`}
-            <br />
-            {`Sie möchten nach Gross-/Kleinschreibung unterscheiden? Dann wählen Sie den Vergleichsoperator "=".`}
-          </li>
-          <li>
-            {`Sie können nach einem Teil des Feldinhalts filtern.`}
-            <br />
-            {`Beispiel: Schreiben Sie "Vogel", wird auch "Eisvogel" gefunden.`}
-            <br />
-            {`Sie möchten nach dem ganzen Feldinhalt filtern? Dann wählen Sie den Vergleichsoperator "=".`}
-          </li>
-          <li>
-            {`Wenn Sie Filter-Werte eingeben, erscheint eine Liste der diese Zeichen enthaltenden Werte dieses Feldes.`}
-            <br />
-            {`Sie können die ungefilterte Liste aller enthaltenen Werte öffnen, indem Sie einen Leerschlag tippen.`}
-          </li>
-        </ul>
-      </StyledCardContent>
-    </Collapse>
-  </StyledCard>
-)
+        <CardActionTitle>Tipps und Tricks</CardActionTitle>
+        <CardActionIconButton
+          data-expanded={expanded}
+          aria-expanded={expanded}
+          aria-label="Show more"
+        >
+          <Icon>
+            <ExpandMoreIcon />
+          </Icon>
+        </CardActionIconButton>
+      </StyledCardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <StyledCardContent>
+          <ul>
+            <li>
+              {`Sie können nach beliebig vielen Eigenschaften filtern. Jedes Kriterium reduziert die Anzahl "Treffer".`}
+              <br />
+              {`Beispiel: Filtern Sie im Namen nach "Eisvogel" und in der Ordnung nach "Lepidoptera", erhalten Sie drei Schmetterlinge aber nicht den entsprechenden Vogel.`}
+            </li>
+            <li>
+              {`Sie möchten nach "Schmetterlinge" oder "Vögel" suchen? Exportieren Sie zuerst "Schmetterlinge", danach die "Vögel" und setzen die zwei Exporte zusammen.`}
+            </li>
+            <li>
+              {`Sie können Vergleichsoperatoren verwenden.`}
+              <br />
+              {`Das Feld, um sie zu wählen erscheint, nachdem ein Filter-Wert erfasst wurde.`}
+            </li>
+            <li>
+              {`Es kommt nicht auf Gross-/Kleinschreibung an.`}
+              <br />
+              {`Beispiel: Schreiben Sie "eisvogel", wird auch "Eisvogel" gefunden.`}
+              <br />
+              {`Sie möchten nach Gross-/Kleinschreibung unterscheiden? Dann wählen Sie den Vergleichsoperator "=".`}
+            </li>
+            <li>
+              {`Sie können nach einem Teil des Feldinhalts filtern.`}
+              <br />
+              {`Beispiel: Schreiben Sie "Vogel", wird auch "Eisvogel" gefunden.`}
+              <br />
+              {`Sie möchten nach dem ganzen Feldinhalt filtern? Dann wählen Sie den Vergleichsoperator "=".`}
+            </li>
+            <li>
+              {`Wenn Sie Filter-Werte eingeben, erscheint eine Liste der diese Zeichen enthaltenden Werte dieses Feldes.`}
+              <br />
+              {`Sie können die ungefilterte Liste aller enthaltenen Werte öffnen, indem Sie einen Leerschlag tippen.`}
+            </li>
+          </ul>
+        </StyledCardContent>
+      </Collapse>
+    </StyledCard>
+  )
+}
 
-export default enhance(Tipps)
+export default Tipps
