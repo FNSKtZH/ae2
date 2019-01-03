64d3142741c6e1954cfd084fb83ffbb418998554
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-03 19:20:42 +0100

    clean

--- src/components/AppBar/index.js
+++ src/components/AppBar/index.js
@@ -6,7 +6,6 @@ import React, {
   useState,
   useCallback,
   useEffect,
-  useLayoutEffect,
 } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
