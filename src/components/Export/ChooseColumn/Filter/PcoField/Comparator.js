ea25e0b4d9b1540b197e97a9d5b1cedff4d200fd
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2019-01-04 16:52:29 +0100

    use hooks

--- src/components/Export/ChooseColumn/Filter/PcoField/Comparator.js
+++ src/components/Export/ChooseColumn/Filter/PcoField/Comparator.js
@@ -5,7 +5,7 @@ import InputLabel from '@material-ui/core/InputLabel'
 import FormControl from '@material-ui/core/FormControl'
 import styled from 'styled-components'
 import compose from 'recompose/compose'
-import { withApollo } from 'react-apollo'
+import { useApolloClient } from 'react-apollo-hooks'
 
 import exportPcoFiltersMutation from '../../../exportPcoFiltersMutation'
 import ComparatorSelect from '../ComparatorSelect'
@@ -36,10 +36,7 @@ const styles = theme => ({
   },
 })
 
-const enhance = compose(
-  withApollo,
-  withStyles(styles),
-)
+const enhance = compose(withStyles(styles))
 
 const PcoComparator = ({
   pcname,
@@ -47,15 +44,14 @@ const PcoComparator = ({
   value,
   comparator,
   classes,
-  client,
 }: {
   pcname: string,
   pname: string,
   value: string,
   comparator: String,
   classes: Object,
-  cliet: Object,
 }) => {
+  const client = useApolloClient()
   const onChange = useCallback(
     event => {
       client.mutate({
