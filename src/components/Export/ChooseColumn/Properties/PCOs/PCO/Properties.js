81fbdc96c0882b730bfe2e87a4545683554d8bdc
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2019-01-06 22:05:52 +0100

    optimize

---
+++ src/components/Export/ChooseColumn/Properties/PCOs/PCO/Properties.js
@@ -0,0 +1,17 @@
+// @flow
+import React from 'react'
+
+import Chooser from './Chooser'
+
+const PCO = ({ properties }: { properties: Array<Object> }) =>
+  properties.map(p => (
+    <Chooser
+      key={`${p.propertyName}${p.jsontype}`}
+      pcname={p.propertyCollectionName}
+      pname={p.propertyName}
+      jsontype={p.jsontype}
+      count={p.count}
+    />
+  ))
+
+export default PCO
