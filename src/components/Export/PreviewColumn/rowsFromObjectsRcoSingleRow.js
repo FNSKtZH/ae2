cf5cb34088dc197b4c0776ce46b08996684286e8
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-07 11:57:33 +0100

    correct error

--- src/components/Export/PreviewColumn/rowsFromObjectsRcoSingleRow.js
+++ src/components/Export/PreviewColumn/rowsFromObjectsRcoSingleRow.js
@@ -15,31 +15,33 @@ export default ({
 }) => {
   // loop through all properties
   exportRcoProperties.forEach(p => {
-    const rcos = thisObjectsRco.filter(r =>
-      get(r, 'propertyCollectionByPropertyCollectionId.name') === p.pcname &&
-      r.relationType === p.relationtype
+    const rcos = thisObjectsRco.filter(
+      r =>
+        get(r, 'propertyCollectionByPropertyCollectionId.name') === p.pcname &&
+        r.relationType === p.relationtype,
     )
     if (p.pname === 'Beziehungspartner_id') {
       // add bp id if wanted
       const vals = rcos
         .map(r => get(r, 'objectByObjectIdRelation.id', null))
         .filter(v => v !== null)
-      row[`${conv(p.pcname)}__${conv(p.relationtype)}__${conv(p.pname)}`] = vals.length ?
-        vals.join(' | ') :
-        null
+      row[
+        `${conv(p.pcname)}__${conv(p.relationtype)}__${conv(p.pname)}`
+      ] = vals.length ? vals.join(' | ') : null
     } else if (p.pname === 'Beziehungspartner_Name') {
       // add bp name if wanted
       const vals = rcos
         .map(r => get(r, 'objectByObjectIdRelation.name', null))
         .filter(v => v !== null)
-      row[`${conv(p.pcname)}__${conv(p.relationtype)}__${conv(p.pname)}`] = vals.length ?
-        vals.join(' | ') :
-        null
+      row[
+        `${conv(p.pcname)}__${conv(p.relationtype)}__${conv(p.pname)}`
+      ] = vals.length ? vals.join(' | ') : null
     } else {
       // add field values
       const vals = rcos
         .map(r => {
-          const rcoProperties = JSON.parse(r.properties)
+          // catch case where properties is null
+          const rcoProperties = r.properties ? JSON.parse(r.properties) : {}
           let val = rcoProperties[p.pname] || null
           if (typeof val === 'boolean') {
             val = booleanToJaNein(val)
@@ -47,9 +49,9 @@ export default ({
           return val
         })
         .filter(v => v !== null)
-      row[`${conv(p.pcname)}__${conv(p.relationtype)}__${conv(p.pname)}`] = vals.length ?
-        vals.join(' | ') :
-        null
-      }
+      row[
+        `${conv(p.pcname)}__${conv(p.relationtype)}__${conv(p.pname)}`
+      ] = vals.length ? vals.join(' | ') : null
+    }
   })
 }
