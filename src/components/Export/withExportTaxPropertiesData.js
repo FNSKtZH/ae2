8d39c24b7d0faa621b7c5535b1189086871a2d18
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2019-01-08 20:23:16 +0100

    remove unused

--- src/components/Export/withExportTaxPropertiesData.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import { graphql } from 'react-apollo'
-
-import exportTaxPropertiesGql from './exportTaxPropertiesGql'
-
-export default graphql(exportTaxPropertiesGql, {
-  name: 'exportTaxPropertiesData',
-})
