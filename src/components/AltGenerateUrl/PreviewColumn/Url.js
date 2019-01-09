b327e600925a926f9a1da83f3443a47f65248986
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-09 19:25:34 +0100

    use hooks

--- src/components/AltGenerateUrl/PreviewColumn/Url.js
+++ src/components/AltGenerateUrl/PreviewColumn/Url.js
@@ -1,13 +1,12 @@
 // @flow
-import React, { Fragment } from 'react'
-import { Query } from 'react-apollo'
-import gql from 'graphql-tag'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import copy from 'copy-to-clipboard'
 import compose from 'recompose/compose'
-import withState from 'recompose/withState'
+import { useQuery } from 'react-apollo-hooks'
+import gql from 'graphql-tag'
 
 import ErrorBoundary from '../../shared/ErrorBoundary'
 
@@ -27,106 +26,91 @@ const StyledTextField = styled(TextField)`
   padding: 8px 0 !important;
 `
 
-const enhance = compose(
-  withState('copyButtonText', 'setCopyButtonText', 'url kopieren'),
-)
+const query = gql`
+  {
+    exportTaxProperties @client {
+      taxname
+      pname
+    }
+    exportPcoProperties @client {
+      pcname
+      pname
+    }
+    exportRcoProperties @client {
+      pcname
+      relationtype
+      pname
+    }
+  }
+`
 
-const Url = ({
-  copyButtonText,
-  setCopyButtonText, 
-}:{
-  copyButtonText: String,
-  setCopyButtonText: () => void,
-}) =>
-  <ErrorBoundary>
-    <Container>
-      <Query
-        query={gql`
-          {
-            exportTaxProperties @client {
-              taxname
-              pname
-            }
-            exportPcoProperties @client {
-              pcname
-              pname
-            }
-            exportRcoProperties @client {
-              pcname
-              relationtype
-              pname
-            }
-          }
-        `}
-      >
-        {({ loading, error, data }) => {
-          if (loading) return 'Lade daten...'
-          if (error) return `Fehler: ${error.message}`
-          const {
-            exportTaxProperties,
-            exportPcoProperties,
-            exportRcoProperties,
-          } = data
-          const fieldsChoosen =
-            [
-              ...exportTaxProperties,
-              ...exportPcoProperties,
-              ...exportRcoProperties,
-            ].length > 0
+const enhance = compose()
 
-          if (!fieldsChoosen) {
-            return (
-              <InfoDiv>
-                Sobald eine Eigenschaft gewählt ist, wird hier eine URL
-                generiert.
-              </InfoDiv>
-            )
-          }
-          const taxProps = exportTaxProperties.map(p => ({
-            t: 'tax',
-            n: p.taxname,
-            p: p.pname,
-          }))
-          const pcoProps = exportPcoProperties.map(p => ({
-            t: 'pco',
-            n: p.pcname,
-            p: p.pname,
-          }))
-          const rcoProps = exportRcoProperties.map(p => ({
-            t: 'rco',
-            n: p.pcname,
-            rt: p.relationtype,
-            p: p.pname,
-          }))
-          const props = [...taxProps, ...pcoProps, ...rcoProps]
-          const url = `https://arteigenschaften.ch/api/alt?fields=${JSON.stringify(
-            props
-          )}`
-          return (
-            <Fragment>
-              <StyledTextField
-                label="URL"
-                value={url}
-                multiline
-                fullWidth
-                autoComplete="off"
-                autoCorrect="off"
-                autoCapitalize="off"
-                spellCheck="false"
-              />
-              <StyledButton onClick={() => {
-                  setCopyButtonText('kopiert')
-                  setTimeout(() => setCopyButtonText('url kopieren'), 3000)
-                  copy(url)
-                }}
-              >
-                {copyButtonText}
-              </StyledButton>
-            </Fragment>
-          )
-        }}
-      </Query>
-    </Container>
-  </ErrorBoundary>
+const Url = () => {
+  const [copyButtonText, setCopyButtonText] = useState('url kopieren')
+
+  const { data, loading, error } = useQuery(query, { suspend: false })
+  const { exportTaxProperties, exportPcoProperties, exportRcoProperties } = data
+  const fieldsChoosen =
+    [...exportTaxProperties, ...exportPcoProperties, ...exportRcoProperties]
+      .length > 0
+  const taxProps = exportTaxProperties.map(p => ({
+    t: 'tax',
+    n: p.taxname,
+    p: p.pname,
+  }))
+  const pcoProps = exportPcoProperties.map(p => ({
+    t: 'pco',
+    n: p.pcname,
+    p: p.pname,
+  }))
+  const rcoProps = exportRcoProperties.map(p => ({
+    t: 'rco',
+    n: p.pcname,
+    rt: p.relationtype,
+    p: p.pname,
+  }))
+  const props = [...taxProps, ...pcoProps, ...rcoProps]
+  const url = `https://arteigenschaften.ch/api/alt?fields=${JSON.stringify(
+    props,
+  )}`
+
+  if (!fieldsChoosen) {
+    return (
+      <InfoDiv>
+        Sobald eine Eigenschaft gewählt ist, wird hier eine URL generiert.
+      </InfoDiv>
+    )
+  }
+
+  if (loading) return 'Lade daten...'
+  if (error) return `Fehler: ${error.message}`
+
+  return (
+    <ErrorBoundary>
+      <Container>
+        <StyledTextField
+          label="URL"
+          value={url}
+          multiline
+          fullWidth
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+          spellCheck="false"
+        />
+        <StyledButton
+          onClick={() => {
+            setCopyButtonText('kopiert')
+            setTimeout(() => setCopyButtonText('url kopieren'), 3000)
+            copy(url)
+          }}
+        >
+          {copyButtonText}
+        </StyledButton>
+      </Container>
+    </ErrorBoundary>
+  )
+}
 
 export default enhance(Url)
