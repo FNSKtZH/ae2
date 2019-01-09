21deebaf80a240823a0f60f110bed13ff01c1a33
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-09 19:11:11 +0100

    remove unused

--- src/store/resolvers/exportPcoProperties.js
+++ src/store/resolvers/exportPcoProperties.js
@@ -3,9 +3,7 @@
 import app from 'ampersand-app'
 import gql from 'graphql-tag'
 
-import exportPcoPropertiesGql from '../../components/Export/exportPcoPropertiesGql'
 import exportPcoFiltersGql from '../../components/Export/exportPcoFiltersGql'
-import exportRcoPropertiesGql from '../../components/Export/exportRcoPropertiesGql'
 import exportTooManyPropertiesMutation from '../../components/Export/exportTooManyPropertiesMutation'
 import constants from '../../modules/constants'
 
@@ -13,6 +11,23 @@ const exportTaxPropertiesGql = gql`
   query exportTaxPropertiesQuery {
     exportTaxProperties @client {
       taxname
+      pname
+    }
+  }
+`
+const exportPcoPropertiesGql = gql`
+  query exportPcoPropertiesQuery {
+    exportPcoProperties @client {
+      pcname
+      pname
+    }
+  }
+`
+const exportRcoPropertiesGql = gql`
+  query exportRcoPropertiesQuery {
+    exportRcoProperties @client {
+      pcname
+      relationtype
       pname
     }
   }
