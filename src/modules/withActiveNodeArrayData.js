dcace3aae2122129d40280ba236e9f32699e7181
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-11 22:37:46 +0100

    remove unused

--- src/modules/withActiveNodeArrayData.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import { graphql } from 'react-apollo'
-
-import activeNodeArrayGql from './activeNodeArrayGql'
-
-export default graphql(activeNodeArrayGql, {
-  name: 'activeNodeArrayData',
-})
