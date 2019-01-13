ca8c45fb65a25b2c64bcc646ea2e7a3b94ca2041
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2019-01-13 11:31:41 +0100

    use mobx store

--- src/components/Organisation/PCs.js
+++ src/components/Organisation/PCs.js
@@ -1,13 +1,15 @@
 // @flow
-import React from 'react'
+import React, { useContext } from 'react'
 import get from 'lodash/get'
 import sortBy from 'lodash/sortBy'
 import styled from 'styled-components'
 import { useQuery } from 'react-apollo-hooks'
 import gql from 'graphql-tag'
+import { observer } from 'mobx-react-lite'
 
 import appBaseUrl from '../../modules/appBaseUrl'
 import ErrorBoundary from '../shared/ErrorBoundary'
+import mobxStoreContext from '../../mobxStoreContext'
 
 const Container = styled.div`
   display: flex;
@@ -29,11 +31,6 @@ const StyledA = styled.a`
   text-decoration-style: dotted;
 `
 
-const storeQuery = gql`
-  query activeNodeArrayQuery {
-    activeNodeArray @client
-  }
-`
 const pcsQuery = gql`
   query orgPCsQuery($name: String!) {
     organizationByName(name: $name) {
@@ -50,15 +47,16 @@ const pcsQuery = gql`
 `
 
 const PCs = () => {
-  const { data: storeData } = useQuery(storeQuery, {
-    suspend: false,
-  })
+  const mobxStore = useContext(mobxStoreContext)
+  const { activeNodeArray } = mobxStore
+  const name = activeNodeArray.length > 1 ? activeNodeArray[1] : 'none'
+
   const { data: pcsData, loading: pcsLoading, error: pcsError } = useQuery(
     pcsQuery,
     {
       suspend: false,
       variables: {
-        name: get(storeData, 'activeNodeArray', ['none', 'none'])[1],
+        name,
       },
     },
   )
@@ -98,4 +96,4 @@ const PCs = () => {
   )
 }
 
-export default PCs
+export default observer(PCs)
