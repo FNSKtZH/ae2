e49a9d1f828f10a350122bec719dfa0491591975
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-14 13:54:09 +0100

    move store

--- src/store/defaults.js
+++ src/store/defaults.js
@@ -1,36 +1,23 @@
 // @flow
-import get from 'lodash/get'
 
-export default async idb => {
-  // fetch user from idb
-  const user = await idb.users.toArray()
-
-  const defaults = {
-    exportType: null,
-    exportTaxonomies: [],
-    exportIds: [],
-    exportTaxProperties: [],
-    exportPcoProperties: [],
-    exportRcoProperties: [],
-    exportTaxFilters: [],
-    exportPcoFilters: [],
-    exportRcoFilters: [],
-    exportOnlyRowsWithProperties: true,
-    exportWithSynonymData: true,
-    exportTooManyProperties: false,
-    exportAddFilterFields: true,
-    exportRcoInOneRow: true,
-    treeFilter: {
-      text: '',
-      id: null,
-      __typename: 'TreeFilter',
-    },
-    login: {
-      token: get(user, '[0].token', null),
-      username: get(user, '[0].username', ''),
-      __typename: 'Login',
-    },
-  }
-
-  return defaults
+export default {
+  exportType: null,
+  exportTaxonomies: [],
+  exportIds: [],
+  exportTaxProperties: [],
+  exportPcoProperties: [],
+  exportRcoProperties: [],
+  exportTaxFilters: [],
+  exportPcoFilters: [],
+  exportRcoFilters: [],
+  exportOnlyRowsWithProperties: true,
+  exportWithSynonymData: true,
+  exportTooManyProperties: false,
+  exportAddFilterFields: true,
+  exportRcoInOneRow: true,
+  treeFilter: {
+    text: '',
+    id: null,
+    __typename: 'TreeFilter',
+  },
 }
