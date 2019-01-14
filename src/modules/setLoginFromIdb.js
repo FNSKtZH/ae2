2c89bc2bc749134956f932a806ff16d6f1ffecda
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-14 12:04:06 +0100

    move store

--- src/modules/setLoginFromIdb.js
+++ src/modules/setLoginFromIdb.js
@@ -1,30 +1,19 @@
 // @flow
 import get from 'lodash/get'
 
-import setLoginMutation from './loginMutation'
-
 export default async ({
   client,
   idb,
+  mobxStore,
 }: {
   client: Object,
   idb: Object,
+  mobxStore: Object,
 }): void => {
   const users = await idb.users.toArray()
   const token = get(users, '[0].token')
   const username = get(users, '[0].username')
   if (username && token) {
-    client.mutate({
-      mutation: setLoginMutation,
-      variables: { username, token },
-      optimisticResponse: {
-        setLoginInStore: {
-          username,
-          token,
-          __typename: 'Login',
-        },
-        __typename: 'Mutation',
-      },
-    })
+    mobxStore.login.setLogin({ username, token })
   }
 }
