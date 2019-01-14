3c91ade97ead16639c945409011f9447d09f1546
Alexander Gabriel <alex.barbalex@gmail.com>
2019-01-14 12:03:46 +0100

    move store

--- src/mobxStore/Login.js
+++ src/mobxStore/Login.js
@@ -1,9 +1,16 @@
 import { types } from 'mobx-state-tree'
 
-export default types.model('TreeFilter', {
-  token: types.optional(types.maybeNull(types.string), null),
-  username: types.optional(types.maybeNull(types.string), null),
-})
+export default types
+  .model('TreeFilter', {
+    token: types.optional(types.maybeNull(types.string), null),
+    username: types.optional(types.maybeNull(types.string), null),
+  })
+  .actions(self => ({
+    setLogin({ username, token }) {
+      self.username = username
+      self.token = token
+    },
+  }))
 
 export const defaultValue = {
   token: null,
