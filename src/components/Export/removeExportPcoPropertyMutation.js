4be61e8a132422c2765c1c9dd79348ce0a64e080
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2019-01-15 08:39:28 +0100

    move store

--- src/components/Export/removeExportPcoPropertyMutation.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import gql from 'graphql-tag'
-
-export default gql`
-  mutation removeExportPcoProperty($pcname: String, $pname: String) {
-    removeExportPcoProperty(pcname: $pcname, pname: $pname) @client
-  }
-`
--- src/store/resolvers/index.js
+++ src/store/resolvers/index.js
@@ -2,14 +2,8 @@
 import merge from 'lodash/merge'
 
 import exportTaxProperties from './exportTaxProperties'
-import exportPcoProperties from './exportPcoProperties'
 import exportRcoProperties from './exportRcoProperties'
 import exportTooManyProperties from './exportTooManyProperties'
 
 export default history =>
-  merge(
-    exportTaxProperties,
-    exportPcoProperties,
-    exportRcoProperties,
-    exportTooManyProperties,
-  )
+  merge(exportTaxProperties, exportRcoProperties, exportTooManyProperties)
