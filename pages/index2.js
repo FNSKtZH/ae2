7375cf9af8faabd963d2a1bb0027dbeab7e2d469
Alexander Gabriel <alex.barbalex@gmail.com>
2019-08-19 11:03:13 +0200

    debug

---
+++ pages/index2.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import isUuid from 'is-uuid'
+import { navigate } from 'gatsby'
+import { observer } from 'mobx-react-lite'
+
+import App from '../src/components/App'
+import getUrlForObject from '../src/modules/getUrlForObject'
+import getUrlParamByName from '../src/modules/getUrlParamByName'
+
+const objectQuery = gql`
+  query ObjectQuery($id: UUID!, $hasObjectId: Boolean!) {
+    objectById(id: $id) @include(if: $hasObjectId) {
+      id
+      taxonomyByTaxonomyId {
+        id
+        type
+      }
+      objectByParentId {
+        id
+        objectByParentId {
+          id
+          objectByParentId {
+            id
+            objectByParentId {
+              id
+              objectByParentId {
+                id
+                objectByParentId {
+                  id
+                  objectByParentId {
+                    id
+                    objectByParentId {
+                      id
+                      objectByParentId {
+                        id
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const Router = () => {
+  /**
+   * check if old url was passed that contains objectId-Param
+   * for instance: from artenlistentool like this:
+   * /index.html?id=AD0B10AA-707D-42C6-B68D-8F88CCD2F0B3
+   */
+  console.log('Router')
+  //return <div>hello</div>
+  const idParam = getUrlParamByName('id')
+  const objectId =
+    idParam && isUuid.anyNonNil(idParam) ? idParam.toLowerCase() : null
+  /**
+   * redirect /index.html?exportieren_fuer_artenlistentool=true
+   * to /artenlistentool/waehlen
+   */
+  const altUrlGenParam = getUrlParamByName('exportieren_fuer_artenlistentool')
+  if (altUrlGenParam) {
+    console.log(
+      'Router: redirecting to /artenlistentool/waehlen. objectId:',
+      objectId,
+    )
+    navigate('/artenlistentool/waehlen')
+  }
+
+  const hasObjectId = !!objectId
+  const { loading, error, data } = useQuery(objectQuery, {
+    variables: { id: objectId, hasObjectId },
+  })
+
+  if (hasObjectId) {
+    if (loading) return 'Loading...'
+    if (error) return `Fehler: ${error.message}`
+    // if idParam was passed, open object
+    const url = getUrlForObject(data.objectById)
+    navigate(`/${url.join('/')}`)
+    return <App />
+  }
+  return <App />
+}
+
+export default observer(Router)
