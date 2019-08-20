b7b32191a1b4dcb119ca011b0d5b9d5963a57cb5
Alexander Gabriel <alex.barbalex@gmail.com>
2019-08-20 11:06:35 +0200

    set active node array on pathname changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import isUuid from 'is-uuid'
@@ -8,6 +8,8 @@ import { observer } from 'mobx-react-lite'
 import App from '../components/App'
 import getUrlForObject from '../modules/getUrlForObject'
 import getUrlParamByName from '../modules/getUrlParamByName'
+import mobxStoreContext from '../mobxStoreContext'
+import getActiveNodeArrayFromPathname from '../modules/getActiveNodeArrayFromPathname'
 
 const objectQuery = gql`
   query ObjectQuery($id: UUID!, $hasObjectId: Boolean!) {
@@ -49,13 +51,17 @@ const objectQuery = gql`
 `
 
 const Router = ({ location }) => {
+  const mobxStore = useContext(mobxStoreContext)
+  // set activeNodeArray when pathname changes
+  const { setActiveNodeArray } = mobxStore
+  useEffect(() => {
+    setActiveNodeArray(getActiveNodeArrayFromPathname())
+  }, [location.pathname, setActiveNodeArray])
   /**
    * check if old url was passed that contains objectId-Param
    * for instance: from artenlistentool like this:
    * /index.html?id=AD0B10AA-707D-42C6-B68D-8F88CCD2F0B3
    */
-  console.log('index.js, location:', location)
-  //return <div>hello</div>
   const idParam = getUrlParamByName('id')
   const objectId =
     idParam && isUuid.anyNonNil(idParam) ? idParam.toLowerCase() : null
