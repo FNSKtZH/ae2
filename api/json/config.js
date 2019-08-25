042bdb3aec1f0df102416abc06e73165d8adb756
Alexander Gabriel <alex.barbalex@gmail.com>
2019-08-25 21:15:41 +0200

    work on api

---
+++ api/json/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  connectionString: `postgres://${process.env.DBUSER}:${process.env.DBPASS}@localhost:5432/ae`,
+}
--- src/pages/test.js
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export default () => <div>hello from test</div>
