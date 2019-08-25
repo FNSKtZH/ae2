079ff31582acca0941160afc8bcda42060247ad0
Alexander Gabriel <alex.barbalex@gmail.com>
2019-08-25 13:25:06 +0200

    add 404

---
+++ src/pages/404.js
@@ -0,0 +1,66 @@
+import React, { useCallback } from 'react'
+import Typography from '@material-ui/core/Typography'
+import { navigate } from 'gatsby'
+import styled from 'styled-components'
+import Button from '@material-ui/core/Button'
+import ErrorBoundary from 'react-error-boundary'
+
+import AppBar from '../components/AppBar'
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+const TextContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  font-weight: 700 !important;
+  text-shadow: 2px 2px 3px white, -2px -2px 3px white, 2px -2px 3px white,
+    -2px 2px 3px white;
+`
+const PageTitle = styled(Typography)`
+  font-size: 2em !important;
+  padding: 15px;
+  font-weight: 700 !important;
+`
+const Text = styled(Typography)`
+  font-size: 1.5em !important;
+  padding: 15px;
+  font-weight: 700 !important;
+`
+const StyledButton = styled(Button)`
+  text-shadow: 2px 2px 3px white, -2px -2px 3px white, 2px -2px 3px white,
+    -2px 2px 3px white;
+  border-color: white !important;
+  margin-top: 10px !important;
+`
+
+const FourOFour = () => {
+  const onClickBack = useCallback(() => navigate('/'), [])
+
+  return (
+    <ErrorBoundary>
+      <Container>
+        <AppBar />
+        <TextContainer>
+          <PageTitle align="center" variant="h6">
+            Oh je
+          </PageTitle>
+        </TextContainer>
+        <TextContainer>
+          <Text align="center" variant="h6">
+            Diese Seite ist nicht verfügbar.
+          </Text>
+        </TextContainer>
+        <TextContainer>
+          <StyledButton variant="outlined" onClick={onClickBack}>
+            Zurück zur Startseite
+          </StyledButton>
+        </TextContainer>
+      </Container>
+    </ErrorBoundary>
+  )
+}
+
+export default FourOFour
