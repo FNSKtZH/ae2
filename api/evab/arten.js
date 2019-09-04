8cf07b1942f0674da172df6db81599002586137a
Alexander Gabriel <alex.barbalex@gmail.com>
2019-09-04 19:53:31 +0200

    promisify

--- api/evab/arten.js
+++ api/evab/arten.js
@@ -3,7 +3,8 @@ const db = pgp(
   `postgres://${process.env.DBUSER}:${process.env.DBPASS}@api.artdaten.ch:5432/ae`,
 )
 
-module.exports = async (req, res) => {
-  const result = await db.any('select * from ae.evab_arten')
-  res.json(result)
+module.exports = (req, res) => {
+  db.any('select * from ae.evab_arten').then(result => {
+    res.json(result)
+  })
 }
