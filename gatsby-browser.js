3ca4688f8b1e4b560c78645d63b2ee11b9b6819d
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2019-11-24 19:30:21 +0100

    ask before updating

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,4 +10,14 @@ import App from './src/App'
 export const wrapRootElement = ({ element }) => <App element={element} />
 
 // https://github.com/gatsbyjs/gatsby/issues/9087#issuecomment-459105021
-export const onServiceWorkerUpdateReady = () => window.location.reload(true)
+export const onServiceWorkerUpdateReady = () => {
+  // clear local storage in case db structure was changed
+  localForage.clear()
+  if (
+    window.confirm(
+      'vermehrung neu laden, um die neuste Version zu installieren?',
+    )
+  ) {
+    window.location.reload(true)
+  }
+}
