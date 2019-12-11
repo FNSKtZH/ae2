f9aa14a307d6c402d8e157129b457371a11ca30f
Alexander Gabriel <alex.barbalex@gmail.com>
2019-12-11 14:19:22 +0100

    work on importing sisf3

--- src/sql/2019-12-10_sisf3prov.sql
+++ src/sql/2019-12-10_sisf3prov.sql
@@ -16,9 +16,17 @@ CREATE TABLE ae.tmp_object (
   ist_teil_von integer default null
 );
 -- 3. import data (importdata.csv)
+-- 3.1 simplify
+update ae.tmp_object
+set quelle = 'Gattung'
+where quelle = 'Gattung / genre';
+update ae.tmp_object
+set quelle = 'Familie'
+where quelle = 'Familie / famille';
 -- 4. add extra fields
 alter table ae.tmp_object add column id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc();
 alter table ae.tmp_object add column properties jsonb DEFAULT NULL;
+alter table ae.tmp_object add column parent_id UUID DEFAULT NULL;
 -- 5. set properties
 update ae.tmp_object set properties = json_build_object(
   'Taxonomie ID intern', tax_id_intern,
@@ -34,15 +42,67 @@ update ae.tmp_object set properties = json_build_object(
   'Grenzgebiet', grenzgebiet,
   'Ist Teil von: Taxonomie ID', ist_teil_von
 );
--- 6. insert data from tmp table into objects
-insert into ae.object (id,taxonomy_id,name,properties)
-select id,taxonomy_id,name,properties from ae.tmp_object;
+-- 6. build hierarchy
+-- 6.1 ist teil von
+update ae.tmp_object
+set parent_id = teile_von.parent_id
+from (
+  select
+    o1.id,
+    o2.id as parent_id 
+  from ae.tmp_object o1
+  inner join ae.tmp_object o2
+  on o2.tax_id = o1.ist_teil_von
+  where o1.ist_teil_von is not null
+) as teile_von
+where ae.tmp_object.id = teile_von.id;
 
--- TODO: add synonyms for all sisf2 objects with same taxonomie id
+-- 6.2 other quelle = Checklist 2017
+-- NOPE: data is not consistent. See gen Aster vs. Aster alpinis for example
+/*update ae.tmp_object
+set parent_id = checklist.parent_id
+from (
+  select
+    id,
+    (
+      select id
+      from ae.tmp_object o1
+      where
+        o1.tax_id_intern < ae.tmp_object.tax_id_intern
+        and o1.quelle = 'Gattung'
+      order by
+        o1.tax_id_intern desc
+      limit 1
+    ) as parent_id
+  from 
+    ae.tmp_object
+  where 
+    parent_id is null
+    and quelle = 'Checklist 2017'
+) as checklist
+where ae.tmp_object.id = checklist.id;*/
+
+
+-- 6.3 Familie and Gattung are numbered wildly. So cannot connect to them > delete them
+delete from ae.tmp_object WHERE quelle = 'Gattung';
+delete from ae.tmp_object WHERE quelle = 'Familie';
+
+-- empty objects of this taxonomy (just in case...)
+delete from ae.object where taxonomy_id = 'c87f19f2-1b77-11ea-8282-bbc40e20aff6';
+
+-- 7. insert data from tmp table into objects
+insert into ae.object (id,taxonomy_id,name,properties,parent_id)
+select id,taxonomy_id,name,properties,parent_id from ae.tmp_object;
+
+-- 8. TODO: add synonyms for all sisf2 objects according to SISF2_vs_Checklist2017
+-- 8.1 import the data into tmp_synonym
+
+
 -- TODO: add synonyms for all synonyms of the same object in sisf2
 -- TODO: add synonyms for all sisf2 fns-objects that are genus
 -- TODO: add ZH GIS property_collection
 -- TODO: drop ae.tmp_object
+-- TODO: drop ae.tmp_synonym
 -- alter EvAB api:
 --   deliver all from sisf3 with status a
 --   deliver those from sisf3 with same taxonomie id in sisf2 with guid of sisf2 object
