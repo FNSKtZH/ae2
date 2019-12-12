659a1741c0d2708cd9fffc76481b71d077c7b3ac
Alexander Gabriel <alex.barbalex@gmail.com>
2019-12-12 21:19:05 +0100

    up version

--- src/components/Layout/AppBar/MoreMenu.js
+++ src/components/Layout/AppBar/MoreMenu.js
@@ -109,7 +109,7 @@ const MoreMenu = () => {
           <br />
           inkl. Schnittstellen-Dokumentation
         </TwoLineMenuItem>
-        <Version>Version: 1.4.2 vom 12.12.2019</Version>
+        <Version>Version: 1.4.3 vom 12.12.2019</Version>
       </Menu>
     </div>
   )
