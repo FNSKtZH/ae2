f927417ab7cbbc6928b7517f68593695bb9b3384
Alexander Gabriel <alex.barbalex@gmail.com>
2019-12-12 16:31:32 +0100

    configure json api

--- jsonApi/config.js
+++ jsonApi/config.js
@@ -1,4 +1,4 @@
-const passfile = require(`./dbPass.json`)
+const passfile = require(`./dbPass.json.js`)
 
 module.exports = {
   connectionString: `postgres://${passfile.user}:${passfile.pass}@localhost:5432/ae`,
