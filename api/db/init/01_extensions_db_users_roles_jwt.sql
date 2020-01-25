8357ca56ee031b809af9c947c937772afad99580
Alexander Gabriel <alex.barbalex@gmail.com>
2020-01-25 14:34:29 +0100

    optimize

--- api/db/init/01_extensions_db_users_roles_jwt.sql
+++ api/db/init/01_extensions_db_users_roles_jwt.sql
@@ -2,9 +2,10 @@
 --CREATE DATABASE ae encoding 'UTF8';
 -- create ae only if not yet exists
 -- see: https://stackoverflow.com/a/18389184/712005
-SELECT 'CREATE DATABASE ae' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ae')\gexec
+-- BUT: it is not necessary to create db ae because was created by postgres because env variable POSTGRES_DB=ae
+--SELECT 'CREATE DATABASE ae' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ae')\gexec
 CREATE EXTENSION if not exists "uuid-ossp";
-create extension if not exists "postgres_fdw";
+create extension if not exists postgres_fdw;
 create extension if not exists pgcrypto;
 create role anon;
 create role authenticator with login password '${AUTHENTICATOR_PASSWORD}' noinherit;
