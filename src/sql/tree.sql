66b9e9218f9b85bf527c53337ff7d97c91cc3aec
Alexander Gabriel <alex.barbalex@gmail.com>
2020-04-23 15:32:29 +0200

    query tree with a graph

--- src/sql/tree.sql
+++ src/sql/tree.sql
@@ -1,21 +1,13 @@
-select '2aabf183-ad8c-4451-9aed-08ae38f8a73f'::UUID as id, 'Arten'::text as name, null::UUID as parent_id, 1::bigint as recursion_level
-select '3ebc6a58-fe9f-4f46-b22a-b1a1aff45a03'::UUID as id, 'Lebensraum'::text as name, null::UUID as parent_id, 1::bigint as recursion_level
-select '33744e59-1942-4341-8b2d-088d4ac96434'::UUID as id, 'Eigenschaften-Sammlungen'::text as name, null::UUID as parent_id, 1::bigint as recursion_level
-
--- STOP
--- TODO: 
--- create table tree_category
--- add parent_id or tree_category to ae.taxonomy
--- use these instead
-
 with
-taxonomies as (
-  select id, name, '2aabf183-ad8c-4451-9aed-08ae38f8a73f'::UUID as parent_id, 2::bigint recursion_level
-  from ae.taxonomy
-  where type = 'Art'
-  order by name
+tree_categories as (
+  select id, name, null::UUID as parent_id, 1::bigint recursion_level
+  from ae.tree_category
 ),
-objects as (
+taxonomies as (
+  select id, name, tree_category as parent_id, 2::bigint recursion_level
+  from ae.taxonomy
+),
+art_objects as (
   with recursive a as (
     select ae.object.id, ae.object.name, ae.object.parent_id, 3::bigint recursion_level
     from ae.object
@@ -37,26 +29,7 @@ objects as (
   select id, name, parent_id, recursion_level
   from a
 ),
-tax_and_objects as (
-  select id, name, parent_id, recursion_level
-  from objects
-  union all
-  select id, name, parent_id, recursion_level
-  from taxonomies
-)
-select id, name, parent_id, recursion_level
-from tax_and_objects
-order by recursion_level, name;
-
-
-with
-taxonomies as (
-  select id, name, '3ebc6a58-fe9f-4f46-b22a-b1a1aff45a03'::UUID as parent_id, 2::bigint recursion_level
-  from ae.taxonomy
-  where type = 'Lebensraum'
-  order by name
-),
-objects as (
+lr_objects as (
   with recursive a as (
     select ae.object.id, ae.object.name, ae.object.parent_id, 3::bigint recursion_level
     from ae.object
@@ -80,11 +53,18 @@ objects as (
 ),
 tax_and_objects as (
   select id, name, parent_id, recursion_level
-  from objects
+  from art_objects
+  union all
+  select id, name, parent_id, recursion_level
+  from lr_objects
   union all
   select id, name, parent_id, recursion_level
   from taxonomies
+  union all
+  select id, name, parent_id, recursion_level
+  from tree_categories
 )
 select id, name, parent_id, recursion_level
 from tax_and_objects
 order by recursion_level, name;
+
