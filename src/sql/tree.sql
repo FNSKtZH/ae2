9e29dbb4b23a7bec81178f36f2666b40b9e8c566
Alexander Gabriel <alex.barbalex@gmail.com>
2020-04-23 19:22:17 +0200

    optimize

--- src/sql/tree.sql
+++ src/sql/tree.sql
@@ -1,6 +1,7 @@
 /**
  * this query is currently not in use
- * would be a very nice way to fetch data for the tree
+ * would be a very nice way to fetch data for the tree:
+ * filter by level and category
  * data for users and organizations could be added
  * ISSUE: if used as view policies would not be respected :-(
  * maybe it could be used as a function returning a set of a type (to be created)
@@ -8,71 +9,81 @@
  */
 with
 tree_categories as (
-  select id, name, null::UUID as parent_id, 1::bigint recursion_level
+  select id, name, null::UUID as parent_id, 1::bigint as level, name as category
   from ae.tree_category
 ),
 taxonomies as (
-  select id, name, tree_category as parent_id, 2::bigint recursion_level
+  select ae.taxonomy.id, ae.taxonomy.name, ae.taxonomy.tree_category as parent_id, 2::bigint as level, ae.tree_category.name as category
   from ae.taxonomy
+  inner join ae.tree_category
+  on ae.taxonomy.tree_category = ae.tree_category.id
 ),
 art_objects as (
   with recursive a as (
-    select ae.object.id, ae.object.name, ae.object.parent_id, 3::bigint recursion_level
+    select ae.object.id, ae.object.name, ae.object.parent_id, 3::bigint as level, ae.tree_category.name as category
     from ae.object
     inner join ae.taxonomy
+      inner join ae.tree_category
+      on ae.taxonomy.tree_category = ae.tree_category.id
     on ae.object.taxonomy_id = ae.taxonomy.id
     where 
       ae.object.parent_id is null
       and ae.taxonomy.type = 'Art'
     union all
-    select o.id, o.name, o.parent_id, a.recursion_level +1
+    select o.id, o.name, o.parent_id, a.level +1, ae.tree_category.name as category
     from ae.object o
       inner join ae.taxonomy
+        inner join ae.tree_category
+        on ae.taxonomy.tree_category = ae.tree_category.id
       on o.taxonomy_id = ae.taxonomy.id
     join a on a.id = o.parent_id
     where 
       ae.taxonomy.type = 'Art'
-      and a.recursion_level <= 10
+      and a.level <= 10
   )
-  select id, name, parent_id, recursion_level
+  select id, name, parent_id, level, category
   from a
 ),
 lr_objects as (
   with recursive a as (
-    select ae.object.id, ae.object.name, ae.object.parent_id, 3::bigint recursion_level
+    select ae.object.id, ae.object.name, ae.object.parent_id, 3::bigint as level, ae.tree_category.name as category
     from ae.object
     inner join ae.taxonomy
+      inner join ae.tree_category
+      on ae.taxonomy.tree_category = ae.tree_category.id
     on ae.object.taxonomy_id = ae.taxonomy.id
     where 
       ae.object.parent_id is null
       and ae.taxonomy.type = 'Lebensraum'
     union all
-    select o.id, o.name, o.parent_id, a.recursion_level +1
+    select o.id, o.name, o.parent_id, a.level +1, ae.tree_category.name as category
     from ae.object o
       inner join ae.taxonomy
+        inner join ae.tree_category
+        on ae.taxonomy.tree_category = ae.tree_category.id
       on o.taxonomy_id = ae.taxonomy.id
     join a on a.id = o.parent_id
     where 
       ae.taxonomy.type = 'Lebensraum'
-      and a.recursion_level <= 10
+      and a.level <= 10
   )
-  select id, name, parent_id, recursion_level
+  select id, name, parent_id, level, category
   from a
 ),
 tax_and_objects as (
-  select id, name, parent_id, recursion_level
+  select id, name, parent_id, level, category
   from art_objects
   union all
-  select id, name, parent_id, recursion_level
+  select id, name, parent_id, level, category
   from lr_objects
   union all
-  select id, name, parent_id, recursion_level
+  select id, name, parent_id, level, category
   from taxonomies
   union all
-  select id, name, parent_id, recursion_level
+  select id, name, parent_id, level, category
   from tree_categories
 )
-select id, name, parent_id, recursion_level
+select level, category, name, id, parent_id
 from tax_and_objects
-order by recursion_level, name;
+order by level, category, name;
 
