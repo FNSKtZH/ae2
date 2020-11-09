57cb852697db0340725d09f84422eb3ac39fdf15
Alexander Gabriel <alex.barbalex@gmail.com>
2020-11-09 16:18:32 +0100

    lint

--- src/components/Export/PreviewColumn/rowsFromObjects.js
+++ src/components/Export/PreviewColumn/rowsFromObjects.js
@@ -11,7 +11,7 @@ import conv from '../../../modules/convertExportFieldName'
 import rowsFromObjectsRcoSingleRow from './rowsFromObjectsRcoSingleRow'
 import rowsFromObjectsRcoMultipleRows from './rowsFromObjectsRcoMultipleRows'
 
-export default ({
+const rowsFromObjects = () => ({
   objects,
   taxProperties,
   withSynonymData,
@@ -28,16 +28,16 @@ export default ({
   // need taxFields to filter only data with properties
   const taxFields = [
     'id',
-    ...taxProperties.map(p => `${conv(p.taxname)}__${conv(p.pname)}`),
+    ...taxProperties.map((p) => `${conv(p.taxname)}__${conv(p.pname)}`),
   ]
   const aditionalRows = []
   //console.log('rowsFromObjects 1:',{rcoInOneRow,rcoProperties,exportOnlyRowsWithProperties})
-  let rows = objects.map(o => {
+  let rows = objects.map((o) => {
     // 1. object
     const row = {}
     row.id = o.id
     const properties = o.properties ? JSON.parse(o.properties) : {}
-    taxProperties.forEach(p => {
+    taxProperties.forEach((p) => {
       let val = null
       if (properties && properties[p.pname] !== undefined) {
         if (typeof properties[p.pname] === 'boolean') {
@@ -55,18 +55,18 @@ export default ({
       // all declared synonyms
       // but only if synonyms data is wanted
       ...synonyms
-        .filter(s => withSynonymData && s.objectId === o.id)
-        .map(s => s.objectIdSynonym),
+        .filter((s) => withSynonymData && s.objectId === o.id)
+        .map((s) => s.objectIdSynonym),
     ]
     // 2. pco
     if (pcoProperties.length > 0) {
-      const thisObjectsPco = pco.filter(p =>
+      const thisObjectsPco = pco.filter((p) =>
         thisObjectsSynonyms.includes(p.objectId),
       )
-      thisObjectsPco.forEach(pco => {
+      thisObjectsPco.forEach((pco) => {
         const properties = JSON.parse(pco.properties)
         if (properties) {
-          pcoProperties.forEach(p => {
+          pcoProperties.forEach((p) => {
             if (properties[p.pname] !== undefined) {
               let val = properties[p.pname]
               if (typeof val === 'boolean') {
@@ -78,7 +78,7 @@ export default ({
         }
       })
       // add every field if still missing
-      pcoProperties.forEach(p => {
+      pcoProperties.forEach((p) => {
         const fieldName = `${conv(p.pcname)}__${conv(p.pname)}`
         if (row[fieldName] === undefined) {
           row[fieldName] = null
@@ -87,7 +87,7 @@ export default ({
     }
     // 3. rco
     if (rcoProperties.length > 0) {
-      const thisObjectsRco = rco.filter(p =>
+      const thisObjectsRco = rco.filter((p) =>
         thisObjectsSynonyms.includes(p.objectId),
       )
       //console.log('rowsFromObjects:',{thisObjectsRco,row})
@@ -119,7 +119,7 @@ export default ({
   })
   rows = [...rows, ...aditionalRows]
   if (exportIds.length > 0) {
-    rows = rows.filter(r => exportIds.includes(r.id))
+    rows = rows.filter((r) => exportIds.includes(r.id))
   }
   // sort by id
   // reason: if multiple rows were created per object,
@@ -127,18 +127,21 @@ export default ({
   rows = sortBy(rows, 'id')
   //console.log('rowsFromObjects 2:',{rows:[...rows]})
 
-  const fields = rows[0] ? Object.keys(rows[0]).map(k => k) : []
-  const propertyFields = fields.filter(f => !taxFields.includes(f))
+  const fields = rows[0] ? Object.keys(rows[0]).map((k) => k) : []
+  const propertyFields = fields.filter((f) => !taxFields.includes(f))
   if (exportOnlyRowsWithProperties && propertyFields.length > 0) {
     // filter rows that only contain values in taxFields
-    rows = rows.filter(row => {
+    rows = rows.filter((row) => {
       // check if any property field contains a value
       const propertyRow = omit(row, taxFields)
-      const valueExists = some(propertyRow, v => v !== undefined && v !== null)
+      const valueExists = some(
+        propertyRow,
+        (v) => v !== undefined && v !== null,
+      )
       return valueExists
     })
   }
-  const pvColumns = fields.map(k => ({
+  const pvColumns = fields.map((k) => ({
     key: k,
     name: k,
     resizable: true,
@@ -147,3 +150,5 @@ export default ({
   //console.log('rowsFromObjects 3:',{rows:[...rows], pvColumns,propertyFields,fields,taxFields})
   return { rows, pvColumns }
 }
+
+export default rowsFromObjects
